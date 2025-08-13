404360f542fb1f1c200b7f414e350291bddb8b40
Kerim <kerim.grbz.01@gmail.com>
2025-08-13 13:04:18 +0200

    classe générique exercice1

--- main.cpp
+++ main.cpp
@@ -3051,13 +3051,62 @@ using namespace std;
 template<typename T>
 class Coord {
 private:
-    T x, y;
+    T x ,y;
+
 public:
-    Coord() : x(T{}, T{}) {}
-    Coord(T x_val, T y_val): x(x_val), y(y_val){}
+    Coord():x(T{}) , y(T{}){}
 
-    // getters
+    Coord(const T& x_val, const T& y_val) : x(x_val), y(y_val){}
 
+    void setCoord(const T& x_val, const T& y_val) {
+        x = x_val;
+        y = y_val;
+    }
 
+    T getX() const {
+        return x;
+    }
+
+    T getY() const {
+        return y;
+    }
+
+    void afficher()const {
+        cout<<"("<<x<<", "<< y<<")";
+    }
+
+    template<typename U>
+    void deplacer(const U& dx, const U& dy) {
+        x += static_cast<T>(dx);
+        y += static_cast<T>(dy);
+    }
 };
+
+int main() {
+    cout << "origine  : ";
+    const Coord<int> origin;
+    origin.afficher();
+    cout << endl;
+
+    cout << "p1       : ";
+    Coord<int> p1;
+    p1.setCoord(1, 2);
+    p1.afficher();
+    cout << endl;
+
+    cout << "p2       : ";
+    Coord<double> p2(3, 4);
+    p2.afficher();
+    cout << endl;
+
+    cout << "p2->     : ";
+    p2.deplacer(1, 1);   // conversion int => double
+    cout << "(" << p2.getX() << ", " << p2.getY() << ")";
+    cout << endl;
+
+    return 0;
+}
+
+
+
 
