1c7a575ca60abc439cf4c50ab3dafa715be0c63f
Kerim <kerim.grbz.01@gmail.com>
2025-09-24 21:11:14 +0200

    if ... else -> switch v3

--- main.cpp
+++ main.cpp
@@ -2,35 +2,16 @@
 using namespace std;
 
 int main() {
-int a, b ,c , r, d, n=5;
-
-if(a>=1) {
-    a =42;
-}else {
-    a *=a;
-}
-
-    if(a ==2) {
-        b= 32;
-    }else if(a<5) {
-        b=12;
+int a;
+    cin>>a;
+    if(a>= 0 && a<6) {
+        cout<<"A";
+    }else if(a<8) {
+        cout<<( a == 6 ? "34" : "4");
     }else {
-        b =23;
+        cout<<"D";
     }
 
-    if(a<0) {
-        if(b<0) {
-            c=a;
-        }else {
-            c= -a;
-        }
-    }else {
-        if(b<5) {
-            c =a+b;
-        }else {
-            c =a-b;
-        }
-    }
 
     return 0;
 }
