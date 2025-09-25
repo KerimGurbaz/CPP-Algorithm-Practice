bf4aa2bc266dc13b5e0593996da2a11da87eee85
Kerim <kerim.grbz.01@gmail.com>
2025-09-25 22:58:43 +0200

    min index..

--- main.cpp
+++ main.cpp
@@ -1,26 +1,24 @@
+
 #include <iostream>
-#include <cmath>
+#include <string>
 using namespace std;
 
-int main() {
-    double  montant_initial;
-    double montant_cible;
-    double taux ;
-    cout<<"Entrez le montant initial > 1000 "<<endl;
-    cin>>montant_initial;
-    cout<<"Entrez le montant cible > 1000000 "<<endl;
-    cin>>montant_cible;
-    cout<<"Entrez le taux d'interet annuel en % > 5"<<endl;
-    cin>>taux;
 
-
-    int n = 0;
-    while(montant_initial < montant_cible) {
-        montant_initial += ( montant_initial *taux )/100;
-        n++;
+char min_car(string& s) {
+    char min_index = 0;
+    for(size_t i = 1;i<s.size(); i++) {
+        if(s[i] < s[min_index]) {
+            min_index = i;
+        }
     }
+    return s[min_index];
+}
 
-    cout<<"Le montant cible est atteint apres "<<n<< "ans.";
-
-
+int main() {
+    std::string str("98716234");
+    std::cout << "str : " << str << std::endl;
+    std::cout << "min : " << min_car(str) << std::endl;
+   //min_car(str) = '5'; // Fonksiyonun dönüş değeriyle string'in içini değiştiriyoruz
+    std::cout << "str : " << str << std::endl;
+    return 0;
 }
