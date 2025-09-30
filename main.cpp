f22bfb63da3b1562b1579c52432b9df3dae9f721
Kerim <kerim.grbz.01@gmail.com>
2025-09-30 10:03:22 +0200

    factorielle et nobre de diviseurs

--- main.cpp
+++ main.cpp
@@ -3,41 +3,51 @@
 #include <iostream>
 #include <string>
 #include <limits>
+#include <cmath>
 
 using namespace std;
 
 int nbDiviseurs(int n) {
+    if(n<= 0) {
+        return -1;
+    }
     int count = 0;
-    for(int i = 1; i<=n ; ++i) {
+    int r = static_cast<int>(sqrt(n));
+
+    for(int i = 1; i<=r ; ++i) {
         if(n%i == 0) {
-            count++;
+            count +=2;
         }
+    }
+    if(r * r == n) {
+        --count;
     }
     return count;
 }
 
 int factorielle(int n) {
-  if(n<0) {
-      return -1;
-  }if(n== 0) {
-      return 1;
-  }
 
-    long long resultat = 1;
-    for(int i = 1; i<=n; ++i) {
-        if(resultat > numeric_limits<int>::max() / i) {
+if(n ==0 || n == 1) {
+    return 1;
+}
+    int result = 1;
+    for(int i = 2; i<=n; ++i) {
+        if(result > numeric_limits<int>::max() / i) {
             return -1;
         }
-        resultat *= i;
+        result *=i;
     }
+    return result;
 }
-
-
 
 int main() {
 
     int result = factorielle(5);
-    cout<<result<<endl;
+    cout << "5! = " << result << '\n';
 
-    return nbDiviseurs(6);
+    int d6 = nbDiviseurs(6);
+    cout << "nbDiviseurs(6) = " << d6 << '\n'; // 1,2,3,6 → 4
+
+
+    return 0;
 }
