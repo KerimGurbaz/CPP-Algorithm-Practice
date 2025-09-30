cb2d31771626de4c995453c1af08b8e135a486cb
Kerim <kerim.grbz.01@gmail.com>
2025-09-30 10:30:56 +0200

    Accroissement d'un avoir bancaire (3)

--- main.cpp
+++ main.cpp
@@ -1,53 +1,43 @@
 
 #include <algorithm>
-#include <iostream>
-#include <string>
-#include <limits>
 #include <cmath>
-
+#include <iostream>
 using namespace std;
 
-int nbDiviseurs(int n) {
-    if(n<= 0) {
-        return -1;
-    }
-    int count = 0;
-    int r = static_cast<int>(sqrt(n));
-
-    for(int i = 1; i<=r ; ++i) {
-        if(n%i == 0) {
-            count +=2;
-        }
-    }
-    if(r * r == n) {
-        --count;
-    }
-    return count;
-}
-
-int factorielle(int n) {
-
-if(n ==0 || n == 1) {
-    return 1;
-}
-    int result = 1;
-    for(int i = 2; i<=n; ++i) {
-        if(result > numeric_limits<int>::max() / i) {
-            return -1;
-        }
-        result *=i;
-    }
-    return result;
-}
 
 int main() {
 
-    int result = factorielle(5);
-    cout << "5! = " << result << '\n';
+    double montant_initial;
+    double taux;
+    int annees;
 
-    int d6 = nbDiviseurs(6);
-    cout << "nbDiviseurs(6) = " << d6 << '\n'; // 1,2,3,6 → 4
+    do {
+        cout<<"Entrez le montant initial > ";
+        cin>>montant_initial;
 
+    }while(montant_initial <1000);
+
+    // Saisie du taux d'intérêt (entre -5 et 50 inclus)
+
+    do {
+        cout<<"Entrez le taux d'interet annuel en % > ";
+        cin>>taux;
+    }while(taux<-5 || taux>50);
+
+    // Saisie du nombre d'années (> 0)
+
+    do {
+        cout<<"Entrez le nobre d'annees > ";
+        cin>>annees;
+    }while(annees <=0);
+
+
+    double montant_final = montant_initial * pow(1 + taux/100.0 , annees);
+
+    cout<<"Le montant disponible apres "<<annees
+    <<"an"<<(annees >1 ? "s" : "")
+    <<"est de "<<montant_final<<" CHF"<< endl;
 
     return 0;
+
 }
