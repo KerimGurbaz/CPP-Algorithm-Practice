a90dc4d02be8908ccf49aad4646ca5d9e121407c
Kerim <kerim.grbz.01@gmail.com>
2025-10-01 23:47:41 +0200

    certains exercices v2

--- main.cpp
+++ main.cpp
@@ -4,50 +4,37 @@
 #include <vector>
 using namespace std;
 
-int nbDiviseurs(int n) {
-    int limit = sqrt(n);
-    int count =0;
-    for(int i=1; i<=limit; ++i) {
-        if(n%i==0) {
-            count+=2;
-        }
-        if(i*i == n) {
-            --count;
-        }
-    }
-    return count;
-}
 
-int hb(int n) {
-   int res = 0;
-    vector<int>v;
-    int precedent = 0;
-    int i =1;
-    while(v.size()!=n) {
-         if(nbDiviseurs(i) > precedent) {
-         precedent = nbDiviseurs(i);
-         v.push_back(i);
-     }
-        ++i;
-    }
-    for(int i = 0; i<v.size(); ++i) {
-        cout<<v[i];
-        if(i!=v.size()-1) {
-            cout<<",";
-        }
-    }
 
-    return v[v.size()-1];
-}
 
 
 
 int main() {
 
-  cout<<nbDiviseurs(6)<<endl;
-    int hb;
-    cout<<"Combien de nombres HC ?";
-    cin>>hb;
+
+    double d;
+    const int I = 5;
+    const double D = 5;
+
+    int i =1;
+
+
+       do {
+         cout<<i;
+           i+=2;
+       }while(i < 10);
+
+    int n = 5;
+    while(n>0) {
+        cout<<n<<" ";
+        if(n == 3) {
+            break;
+        }
+        --n;
+    }
+
+
+
 
 
 
