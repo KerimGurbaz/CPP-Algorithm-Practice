d9a85104bc70928edd26bc3628d5cac8d78e2f5a
Kerim <kerim.grbz.01@gmail.com>
2025-10-03 10:34:08 +0200

    example v3

--- main.cpp
+++ main.cpp
@@ -4,54 +4,28 @@
 #include <vector>
 using namespace std;
 
-void echanger(int &a, int& b) {
-    int temp = b;
-    b = a;
-    a = temp;
-}
-
-int min(int &a, int &b) {
-    if (a<b) {
-        return b;
+int premier_e(string_view s) {
+    for(int i = 0; i<s.size(); ++i) {
+        if(s[i]== 'e' || s[i]== 'E') return i;
     }
-    return b;
+    return -1;
 }
 
-void fonction_A(int& ref);       // Attend une référence non-constante
-void fonction_B(const int& ref); // Attend une référence constante
+void echanger(int& a, int &b) {
+    int temp = a;
+    a =b;
+    b=temp;
+}
 
 int main() {
-    int x =3;
-    int y = 4;
-cout<<x<<" "<<y<<endl;
 
+    cout<<premier_e("Bonjour")<<endl;
+    cout<<premier_e("Renverser")<<endl;
+    cout<<premier_e("Elan")<<endl;
+
+    int x = 3, y = 5;
     echanger(x, y);
-    cout<<x<<" "<<y<<endl;
-    cout<<min(145, 30);
+    cout << x << " " << y << endl;
 
-
-    int a = 3, b = 5;
-
-    // Affiche : a=3, b=5
-    std::cout << "Avant: a=" << a << ", b=" << b << std::endl;
-
-    // La ligne qui pose problème
-    min(a, b), std::max(a, b);
-
-    // Devrait afficher : a=5, b=3
-    std::cout << "Après: a=" << a << ", b=" << b << std::endl;
-
-    int i;
-    double d;
-    const int I = 5;
-    const double D = 5;
-
-    int *const ptr1 = &i;
-    double *const ptr2 =&d;
-    cout<<*ptr1<<endl;;
-    cout<<&d<<endl;
-    cout<<ptr2;
-    *ptr1 = 3;
-const double &ref_d = d;
     return 0;
 }
