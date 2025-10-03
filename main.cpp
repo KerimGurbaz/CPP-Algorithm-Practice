209903649680c025feec8f08acbfe805de74ae64
Kerim <kerim.grbz.01@gmail.com>
2025-10-03 10:17:21 +0200

    Permutation circulaire droite de 3 réels v2

--- main.cpp
+++ main.cpp
@@ -4,49 +4,54 @@
 #include <vector>
 using namespace std;
 
-bool estBissextile (int n) {
-    if(n<1) {
-        return false;
-    }
-    return (n%400 ==0 || (n%4 ==0 && n%100 !=0));
-}
-
-bool listerCaracteres(char debut, char fin) {
-    if(debut > fin) {
-        return false;
-    }
-    for(char c = debut; c <= fin; ++c) {
-        cout<<c;
-    }
-    return true;
-}
-
-void mystere(int& x, int& y) {
-    int temp = y;
-    y =x;
-    x =temp;
-}
-
-void permutation_circulaire(int &a, int &b, int&c) {
-    int temp =c;
-    c=b;
+void echanger(int &a, int& b) {
+    int temp = b;
     b = a;
-    a= temp;
+    a = temp;
 }
 
+int min(int &a, int &b) {
+    if (a<b) {
+        return b;
+    }
+    return b;
+}
+
+void fonction_A(int& ref);       // Attend une référence non-constante
+void fonction_B(const int& ref); // Attend une référence constante
 
 int main() {
+    int x =3;
+    int y = 4;
+cout<<x<<" "<<y<<endl;
 
-    cout<<boolalpha<<estBissextile(2020)<<endl;
-
-    cout<<listerCaracteres(65, 67) <<endl;
-
-    int x = 3, y = 4;
-    mystere(x, y);
-    cout << "x = " << x << " " << "y = " << y << endl;
-    return EXIT_SUCCESS;
+    echanger(x, y);
+    cout<<x<<" "<<y<<endl;
+    cout<<min(145, 30);
 
 
+    int a = 3, b = 5;
 
+    // Affiche : a=3, b=5
+    std::cout << "Avant: a=" << a << ", b=" << b << std::endl;
+
+    // La ligne qui pose problème
+    min(a, b), std::max(a, b);
+
+    // Devrait afficher : a=5, b=3
+    std::cout << "Après: a=" << a << ", b=" << b << std::endl;
+
+    int i;
+    double d;
+    const int I = 5;
+    const double D = 5;
+
+    int *const ptr1 = &i;
+    double *const ptr2 =&d;
+    cout<<*ptr1<<endl;;
+    cout<<&d<<endl;
+    cout<<ptr2;
+    *ptr1 = 3;
+const double &ref_d = d;
     return 0;
 }
