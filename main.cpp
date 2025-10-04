21d809a2a4b4c966e1468cb326b1512e2244a20a
Kerim <kerim.grbz.01@gmail.com>
2025-10-04 16:04:21 +0200

    Factorielle avec contrôle d’overflow

--- main.cpp
+++ main.cpp
@@ -1,101 +1,39 @@
-#include <iostream>
+#include <cstdlib>
 #include <cmath>
-#include <cctype>
+#include <iostream>
+
 using namespace std;
+int facto(int n) {
+    int result = 1;
+    if(n<0) {
+        cout<<"n ne doit etre inferioer de zero";
+    }else if(n ==0 || n==1) {
+        return 1;
+    }else {
 
-enum class StatutOperation {
-    Succes, Erreur_operateur_invalide, Error_division_par_zero
-};
-
-StatutOperation calculer(double a, double b, char op, double& resultat) {
-    switch(op) {
-        case '+' :
-            resultat = a+b;
-        return StatutOperation::Succes;
-        case '-':
-            resultat = a-b;
-        return StatutOperation::Succes;
-        case '*':
-            resultat = a*b;
-        return StatutOperation::Succes;
-        case '/':
-            if(b == 0) {
-                return StatutOperation::Error_division_par_zero;
+        for(int i =1; i<=n; ++i) {
+            if(result > numeric_limits<int>::max()/i) {
+                return -1;
             }
-        resultat =a/b;
-        return StatutOperation::Succes;
-
-        default:
-            return StatutOperation::Erreur_operateur_invalide;
-
+            result *=i;
+        }
+    }
+    return result;
+}
+int factorielle(int n) {
+    if(n == 0 || n==1) {
+        return 1;
+    }else {
+        return  (n * factorielle(n-1));
     }
 }
 
-bool operation(double a, double b, char c, double &resultat) {
-    switch (c) {
-        case '+': resultat = a + b;
-            return true;
-        case '-': resultat = a - b;
-            return true;
-        case '*': resultat = a * b;
-            return true;
-        case '/':
-            if (b == 0) {
-                cout << "Error, division par zero..";
-                return false;
-            }
-            resultat = a / b;
-            return true;
-        case '^': resultat = pow(a, b);
-            return true;
-        default:
-            cout << "Erreur : operator inconnu( " << c << ")" << endl;
-            return false;
-    }
-}
 
 int main() {
-    double x, y, res1;
-    char op =0;
-    double num1, num2, res;
-    char operateur;
-    StatutOperation statut = calculer(num1, num2, operateur, res);
 
+    int n = 5;
 
-   while(true) {
-       cout<<"Entrez deux nombres (ou Ctrl+D pour quitter) : ";
-       if(!(cin>>x>>y)) {
-           cout<<"\nFin de saisie.\n";
-           break;
-       }
-
-       cout << "Entrez l'operateur (+, -, *, /, ^) ou 'q' pour quitter : ";
-       cin >> op;
-
-       if(tolower(static_cast<unsigned char>(op)) == 'q') {
-           cout<<"Au revoir !"<<endl;
-           break;
-       }
-
-       if(operation(x,y,op, res)) {
-           cout << "Resultat : " << res << endl;
-       }else {
-cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-       }
-return 0;
-   }
-
-    double s, d;
-    while(true) {
-        cout<<"Entez deux numbers : ";
-        if(cin>>x>>y)break;
-        cout<<"invalide entree, repeter svp : ";
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');;
-    }
-
-
-
+    cout<<factorielle(n)<<endl;
+    cout<<facto(n)<<endl;
     return 0;
 }
