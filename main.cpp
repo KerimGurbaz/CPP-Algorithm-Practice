30a705d380c0940f77487a6c010f4090b64b0a58
Kerim <kerim.grbz.01@gmail.com>
2025-10-05 12:29:39 +0200

    compter_voyelles(const string& texte)

--- main.cpp
+++ main.cpp
@@ -1,39 +1,24 @@
-#include <cstdlib>
-#include <cmath>
 #include <iostream>
-
+#include <cctype>
 using namespace std;
-int facto(int n) {
-    int result = 1;
-    if(n<0) {
-        cout<<"n ne doit etre inferioer de zero";
-    }else if(n ==0 || n==1) {
-        return 1;
-    }else {
 
-        for(int i =1; i<=n; ++i) {
-            if(result > numeric_limits<int>::max()/i) {
-                return -1;
-            }
-            result *=i;
-        }
+int compter_voyelles(const string& texte) {
+    string s = texte;
+    int count = 0;
+    for(char c : s) {
+      if(  tolower(c) == 'a' ||tolower(c) == 'e'||tolower(c) ==  'i'||tolower(c) == 'o' ||tolower(c) == 'u'){
+          count++;
+      }
     }
-    return result;
+return count;
 }
-int factorielle(int n) {
-    if(n == 0 || n==1) {
-        return 1;
-    }else {
-        return  (n * factorielle(n-1));
-    }
-}
-
 
 int main() {
 
-    int n = 5;
 
-    cout<<factorielle(n)<<endl;
-    cout<<facto(n)<<endl;
+        string phrase = "Bonjour le Monde";
+        cout << compter_voyelles(phrase);
+
+
     return 0;
 }
