d3498ddfd92218d50be6643b3687fd42accb8bd8
Kerim <kerim.grbz.01@gmail.com>
2025-10-06 12:27:31 +0200

    static int x =)

--- main.cpp
+++ main.cpp
@@ -3,33 +3,19 @@
 
 using namespace std;
 
-int a, b;
+void afficher() {
+    static int compteur;
+    cout<<"Appel numéro "<<++compteur<<endl;
 
-//------------------------------------------------------------
-int f(int c) {
- int n = 0;
- a = c;
- if (n < c) {
-  n = a + b;
- }
- return n;
 }
 
-//------------------------------------------------------------
-int g(int c) {
- int n = 0;
- int a = c;
- if (n < f(c)) {
-  n = a + b;
- }
- return n;
-}
 
-//------------------------------------------------------------
 int main() {
- int i = 1;
- int b = g(i);
- cout << "resultat : " << a + b + i << endl;
 
- return EXIT_SUCCESS;
+    for(int i =1; i<=3; ++i) {
+        afficher();
+
+    }
+
+    return 0;
 }
