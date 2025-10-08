23088c9689e1fdd91dbb9559b2acdd7457e1f18c
Kerim <kerim.grbz.01@gmail.com>
2025-10-08 22:11:12 +0200

    Fonctions & Logique and facto..

--- main.cpp
+++ main.cpp
@@ -1,87 +1,54 @@
 #include <iostream>
+#include <limits>
 using namespace std;
 
-
-
-void valeurAbsolue(int& n) {
-    n =n<0 ? -n : n;
-}
-void carre(int n) {
-    if(n>0) {
-        for(int i = 1; i<=n; ++i) {
-            for(int j = 1; j<=n; ++j) {
-                cout<<"*";
-            }
-            cout<<endl;
+long long int facto(int n) {
+    if(n == 0 || n == 1) {
+        return 1;
+    }
+    long long int result =1;
+    for(int i=2; i<= n; ++i) {
+        if(result > numeric_limits< long long int>::max() / i ) {
+            return -1;
+        }else {
+            result *=i;
         }
+
     }
+    return result;
 }
-
-int compterPairs(int n) {
-    int compte =0;
-    if(n == 0)return 1;
-    if(n<0) {
-        n= abs(n);
-    }
-    while(n>0) {
-        int reste = n%10;
-        if(reste %2 ==0) {
-            compte++;
-        }
-        n/=10;
-    }
-    return  compte;
-}
-bool estBissextile(int annee) {
-    if(annee<0)return false;
-    return annee%400==0 ||( annee%4 ==0 && annee%100!=0);
-}
-
-double retrait(double& solde, double montant) {
-   int retire = montant;
-    if(solde<0.) {
-        return -1;
-    }else if(montant >= solde) {
-         retire = solde;
-        solde =0.;
-    }else {
-        solde -= montant;
-    }
-        return retire;
-
-}
-
-void permuter_gauche(int& a, int& b, int&c) {
-    int temp =a;
-    a =b;
-    b=c;
-    c=temp;
-}
-
-
 int main() {
+int num ;
 
-    int n=-100;
-     valeurAbsolue(n);
-    cout<<n<<endl ;
-    int k =5;
-    carre(k);
-    cout<<compterPairs(245678)<<endl;
-    cout<<estBissextile(2400)<<endl;
+    do {
+        cout<<"Enrez un number entre [0 -50] : "<<endl;
+        cin>>num;
+    }while(num<0 || num>50);
 
+    if(num>1) {
+        if(facto(num)) {
+            cout<<num<<"! = ";
+            for(int i=0; i<=num; ++i) {
+                cout<<i<< (i == num ? " = " : " * ");
 
-    int h;
-    cin>>h;
-    for(int i=1; i<=h; ++i) {
-        for(int es =0; es<i; ++es) {
-            cout<<" ";
+            }
         }
-        for(int e = 1;e<=h-i+1; ++e) {
-            cout<<e;
-        }cout<<endl;
-
     }
+
+
+
+    cout<<facto(num);
+
+    int cnt = 0;
+for ( int i = 0; i < 2; ++i ) {
+ for ( int j = i; j < 3; ++j ) {
+ cnt++;
+ }
+ cnt--;
+}
+cout << cnt;
+
+
 
     return 0;
-
 }
