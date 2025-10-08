504dac47e7ab2dbe68b7edce693e9ea7f5324663
Kerim <kerim.grbz.01@gmail.com>
2025-10-08 23:37:53 +0200

    Fonctions & Logique and facto..int syracuse(int & n) {

--- main.cpp
+++ main.cpp
@@ -1,54 +1,42 @@
 #include <iostream>
 #include <limits>
+#include <cmath>
+#include <cctype>
+
 using namespace std;
 
-long long int facto(int n) {
-    if(n == 0 || n == 1) {
-        return 1;
-    }
-    long long int result =1;
-    for(int i=2; i<= n; ++i) {
-        if(result > numeric_limits< long long int>::max() / i ) {
-            return -1;
-        }else {
-            result *=i;
-        }
-
-    }
-    return result;
+int lire_entier(const string& n) {
+    string s = n;
+    int num =static_cast<int>(s);
+    return num;
 }
+
+int syracuse(int & n) {
+    int count =0;
+if(n>0) {
+
+   do {
+       ++count;
+       if(n%2==0) {
+           n/=2;
+       }else {
+           n = n*3+1;
+       }
+   }while(n!=1);
+}
+    else {
+        cout<<"La valeur n'est pas valide";
+    }
+    return count;
+}
+
+
 int main() {
-int num ;
 
-    do {
-        cout<<"Enrez un number entre [0 -50] : "<<endl;
-        cin>>num;
-    }while(num<0 || num>50);
-
-    if(num>1) {
-        if(facto(num)) {
-            cout<<num<<"! = ";
-            for(int i=0; i<=num; ++i) {
-                cout<<i<< (i == num ? " = " : " * ");
-
-            }
-        }
-    }
-
-
-
-    cout<<facto(num);
-
-    int cnt = 0;
-for ( int i = 0; i < 2; ++i ) {
- for ( int j = i; j < 3; ++j ) {
- cnt++;
- }
- cnt--;
-}
-cout << cnt;
-
-
-
+    const string msg = "Veuillez saisir une valeur : ";
+    int i = lire_entier(msg);
+    cout << "La suite de Syracuse pour n = " << i << " : ";
+    int nb_iteration = syracuse(i);
+    cout << "\nLe nombre d'iterations = " << nb_iteration;
     return 0;
 }
