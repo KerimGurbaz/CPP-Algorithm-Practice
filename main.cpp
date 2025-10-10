ccd2fc9fe8138b67b15bf14a1cc30582f8c1bccb
Kerim <kerim.grbz.01@gmail.com>
2025-10-10 09:52:20 +0200

    Compter les voyelles,Période de la journée,Retrait bancaire Minimum de trois (référence de retour),Suite de Syracuse (Collatz),Somme des diviseurs stricts & parfait v2

--- main.cpp
+++ main.cpp
@@ -157,5 +157,34 @@ int main() {
     cin >> n;
     cout << sommeDiviseursStricts(n) << '\n';
     cout << boolalpha << estParfait(n) << '\n';
+
+
+    for(int i = 1; i<=n; ++i) {
+        for(int j = 1; j<=n-i; ++j) {
+            cout<<" ";
+        }for(int k = 1; k<=2*i-1; ++k) {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+
+    for(int i = 1; i<=n; ++i) {
+        for(int j = 1; j<=n-i; ++j) {
+            cout<<" ";
+        }for(int k = 1; k<=2*i-1; ++k) {
+            cout<<"#";
+        }
+        cout<<endl;
+    }
+    for(int i =1; i<=n/2+1;++i) {
+        for(int j =1; j<=n+i;++j) {
+            cout<<" ";
+        }for(int k =n/2+1; k>=i-1; --k) {
+            cout<<"#";
+        }
+        cout<<endl;
+    }
+
     return 0;
 }
