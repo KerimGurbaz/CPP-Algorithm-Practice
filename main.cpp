687f5cfe38cd61b8762c2d58b3bb321b28355093
Kerim <kerim.grbz.01@gmail.com>
2025-10-11 23:53:15 +0200

    Inverser les Chiffres,Le Plus Grand Commun Diviseur,epurerChaine v2

--- main.cpp
+++ main.cpp
@@ -2,136 +2,121 @@
 * #include <iostream>
 using namespace std;
 
-int inverserNombre(int n) {
-    int newNum=0;
+int inverser_pairs(int n) {
+    int nNum =0;
     while(n>0) {
         int reste = n%10;
-        newNum = newNum * 10 +reste;
+        if(reste %2 ==0) {
+            nNum = nNum * 10 +reste;
+        }
         n/=10;
     }
-    return newNum;
+return nNum;
 }
 
 int main() {
-    cout << "L'inverse de 12345 est : " << inverserNombre(12345) << endl; // Attendu: 54321
-    cout << "L'inverse de 900 est : " << inverserNombre(900) << endl;     // Attendu: 9
+    int n;
+    cin >> n;
+    cout << inverser_pairs(n);
     return 0;
 }
-
-// --- Ne pas modifier ---
 #include <iostream>
 using namespace std;
 
-void trier(int &x,int& y,int & z) {
-    if(x>y) {
-        int temp = y;
-        y=x;
-        x=temp;
-    }if(x>z) {
-        int temp =z;
-        z =x;
-        x=temp;
+int somme_limite(int limite) {
+int n =0;
+    int somme = 0;
+do {
+    somme+=n;
+    if(somme<limite) {
+        ++n;
     }
-    if(y>z) {
-        int temp=z;
-        z =y;
-        y=temp;
-    }
-
-}
-
-
-void swp(int& a, int &b, int & c) {
-    if(a<b) swap(a,b);
-    if(a<c) swap(a,c);
-    if(b<c) swap(b,c);
-}
-
-
-
-int main() {
-    int x = 30, y = 10, z = 20;
-    trier(x, y, z);
-    cout << "Nombres tries : " << x << ", " << y << ", " << z << endl; // Attendu: 10, 20, 30
-
-
-    int a= 40, b = 3210, c= 340;
-    swp(a, b, c);
-    cout << "Nombres tries : " << a<< ", " << b<< ", " << c << endl;
-    return 0;
-}
-// --- Ne pas modifier ---
-#include <iostream>
-using namespace std;
-
-void dessinerZigzag(int h, int l) {
-    for(int i = 1; i<=h; ++i) {
-        for(int j =1 ; j<i%l; ++j) {
-            cout<<" ";
-        }for(int k =0; k<1; ++k) {
-            cout<<"*";
-        }
-        cout<<endl;
-    }
+}while(somme<=limite);
+    return n;
 }
 
 int main() {
-    cout << "Zigzag 10x5:" << endl;
-    dessinerZigzag(10, 5);
+    int n;
+    cin >> n;
+    cout << somme_limite(n);
     return 0;
 }
 
 #include <iostream>
 #include <string>
-#include <cctype> // pour isalpha, isspace
 using namespace std;
 
-void epurerChaine(string& s) {
-    string nStr ="";
-    for(char c : s) {
-        if(isalnum(c)|| isspace(c)) {
-            nStr.push_back(c);
+bool est_palindrome(const string& s) {
+    int first =0;
+    int last = s.size()-1;
+    while(first < last) {
+        if(tolower(s[first]) !=  tolower(s[last])){
+            return false;
         }
+        ++first;
+        --last;
     }
-   s =nStr;
-
+    return true;
 }
 
 int main() {
-    string phrase = "J'ai 2 pommes et 3 poires!!!";
-    epurerChaine(phrase);
-    cout << "Phrase epuree : '" << phrase << "'" << endl; // Attendu: 'Jai pommes et poires'
+    string s;
+    getline(cin, s);
+    cout << boolalpha << est_palindrome(s);
     return 0;
 }
- */
+double moyenne_glissante(int x) {
+    static int count =1;
+    static int somme =0;
+    int moyenne =0;
+    if(x== 0) {
+    count =1;
+   somme =0;
+        moyenne=0;
+    }else {
+        somme += x;
+        moyenne= somme / count;
+        ++count;
+    }
+    return moyenne;
+}
 
-// --- Ne pas modifier ---
-// --- Ne pas modifier ---
+int main() {
+    int n;
+    cout << fixed << setprecision(1);
+    while (cin >> n) {
+        cout << moyenne_glissante(n) << endl;
+    }
+    return 0;
+}
+
+
+ */
 #include <iostream>
 using namespace std;
 
-void compteARebours(int n) {
-    while(n>0) {
-        if(n%15 == 0) {
-            cout<<"TicTac,";
-  n--;
-        }else if(n%5==0) {
-            cout<<"Tic,";
- n--;
-        }else if(n%3 == 0) {
-            cout<<"Tac,";
-         n--;
-        }else {
-            cout<<n--<<",";
-        }
-
+void losange(int h) {
+    int m = h/2+1;
+    for(int i=1; i<=m; ++i) {
+        for(int j =1; j<=m-i; ++j) {
+            cout<<" ";
+        }for(int k = 0; k<2*i-1; ++k) {
+            cout<<"*";
+        }cout<<endl;
+    }for(int i = m-1; i>=1; --i) {
+        for(int j = m-i; j>=1; --j) {
+            cout<<" ";
+        }for(int k =0; k<2*i-1; ++k) {
+            cout<<"*";
+        }cout<<endl;;
     }
 }
 
 int main() {
-    cout << "Compte a rebours depuis 16 :" << endl;
-    compteARebours(16);
+    int h;
+    cin >> h;
+    losange(h);
     return 0;
 }
-// Sortie attendue:
-// 16, TicTac, 14, 13, Tac, 11, Tic, Tac, 8, 7, Tac, Tic, 4, Tac, 2, 1,
+
+
