bc139cb47eddff81d93b2178f433bc5c1090e5f6
Kerim <kerim.grbz.01@gmail.com>
2025-10-11 20:24:50 +0200

    parenthesesValides

--- main.cpp
+++ main.cpp
@@ -1,47 +1,51 @@
+/*
+// --- Ne pas modifier ---
 #include <iostream>
+#include <string>
 using namespace std;
 
-void triangle_creux_centre(int h, char c) {
-    for(int i=1; i<=h; ++i) {
-        for(int j = 1; j<h-i; ++j) {
-            cout<<" ";
-        }
-        if(i==1) {
-            cout<<c;
-        }else if(i==h) {
-            for(int k=0; k<2*i-1; ++k) {
-                cout<<c;
-            }
-        }else {
-            cout<<c;
-            for(int k=1; k<=2*i-3; ++k) {
-                cout<<" ";
-            }
-            cout<<c;
-        }
-        cout<<endl;
-    }
-
+bool parenthesesValides(const string& s) {
+    int count=0;
+  for(char c : s) {
+      if(c =='(') {
+          ++count;
+      }else if(c==')') {
+          --count;
+      }
+  }
+    return count ==0;
 }
 
 int main() {
-    int h; char c;
-    cin >> h >> c;
-    triangle_creux_centre(h, c );
+
+    cout << "()) -> " << boolalpha << parenthesesValides("(())") << endl;         // Attendu: false
     return 0;
 }
 
-/*
-Exemples:
-h=1, c=#
-    #
-h=2, c=#
-   # 
-  ###
-h=5, c=#
-    #
-   # #
-  #   #
- #     #
-#########
+#include <iostream>
+using namespace std;
+
+void dessinerMatrice(int n) {
+    for(int i =0; i<n; ++i) {
+        for(int j = 0; j<n; ++j) {
+            if(i==j) {
+                cout<<"1";
+            }else if(i+j == n-1) {
+                cout<<"2";
+            }else {
+                cout<<"0";
+            }
+        }
+        cout<<endl;
+    }
+}
+
+
+int main() {
+    cout << "Matrice de taille 5:" << endl;
+    dessinerMatrice(5);
+    return 0;
+}
+
 */
+
