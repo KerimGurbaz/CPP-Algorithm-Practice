4ae356aeebfc2774270062037c3f1c433f5b5bf8
Kerim <kerim.grbz.01@gmail.com>
2025-10-11 11:16:38 +0200

    Q7 — triangle_creux(int h)Q6 — moyenne_valide()Q5 — table(int)Q4 — somme_impairs(int)Q3 — compte_a_rebours(int) Q2 — remplir(double& v, double ajout)Q1 — mention(double)

--- main.cpp
+++ main.cpp
@@ -1,165 +1,201 @@
 /*
-* // --- Ne pas modifier ---
-#include <cmath>
+* #include <iostream>
+#include <string>
+using namespace std;
+
+string mention(double n) {
+  do {
+      if(n<0 || n>10) {
+       cout<< "repetez svp.. ";
+          cin>>n;
+      }
+  }while(n<0 || n>10);
+    if(n>=8) {
+        return "excellent";
+    }else if(n>=6.5) {
+       return "très bien";
+    }else if(n>=5.5) {
+     return "bien";
+    }else if(n>=4.5) {
+      return "passable";
+    }else {
+     return "insuffisant";
+    }
+}
+
+int main() {
+    double n;
+    cin >> n;
+    cout << mention(n);
+    return 0;
+}
+
+/*
+Entrée : 4.5 → passable
+Entrée : 8.7 → excellent
+
+
+#include <iostream>
+using namespace std;
+void compte_a_rebours(int n) {
+
+   do {
+       cout<<n--<<" ";
+   }while(n>=0);
+}
+int main() {
+    int n;
+    cin >> n;
+    compte_a_rebours(n);
+    return 0;
+}
+
 #include <iostream>
 using namespace std;
 
-int sommeDiviseur(int &n) {
-    int limit = sqrt(n);
-    int somme =1;
 
-    for(int i =2 ; i<=limit; ++i) {
-        if(n%i == 0) {
-            int d = n/i;
-            somme += (d==i) ? d : d+i;
+/*
+Entrée : 53842 → 5 + 3 = 8
+
+
+
+#include <iostream>
+using namespace std;
+void table(int n) {
+        for(int j=1; j<=10; ++j) {
+            cout<<n<<" x "<<j<<" = "<<n*j<<endl;
         }
+    cout<<endl;
+int x = 1;
+    while(x<11) {
+        cout<<n<<" x "<<x<<" = "<<n*x<<endl;
+        ++x;
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    table(n);
+    return 0;
+}
+
+/*
+Entrée : 3
+Sortie :
+3 x 1 = 3
+3 x 2 = 6
+...
+3 x 10 = 30
+
+
+
+/*
+Entrées : 4 5 12 -3 6 -1
+Sortie : 5.0
+
+
+/*
+Entrée : 5
+*/
+
+
+/*
+*
+double remplir(double&v, double a) {
+    if(v+a>=100) {
+        double ajoutEffectif = 100-v;
+        v=100;
+        return ajoutEffectif;
+    }else {
+        v+=a;
+        return a;
+    }
+}
+
+double remplir(double & v, double ajout) {
+    if(ajout <=0) ajout =0;
+    double libre = 100-v;
+    if(libre <=0)return 0.0;
+    double effectif = (ajout <=libre) ? ajout : libre;
+    v +=effectif;
+    return effectif;
+}
+
+int main() {
+    double v = 90;
+    cout << remplir(v, 2) << " " << v << endl; // 10 100
+    cout << remplir(v, 15) << " " << v << endl; // 10 100
+    cout << remplir(v, 5) << " " << v << endl;  // 0 100
+    return 0;
+}
+
+
+int somme_impairs(int n) {
+    n = n<0 ? abs(n): n;
+    int somme = 0;
+    while(n>0) {
+        int reste = n%10;
+        if(reste%2) {
+            somme+=reste;
+        }
+        n/=10;
     }
     return somme;
 }
 
-bool sontAmicaux(int a, int b) {
-   return  sommeDiviseur(a) == b && sommeDiviseur(b) == a;
-}
-
 int main() {
-
-
-    if (sontAmicaux(220, 284)) {
-        cout << "220 et 284 sont amicaux." << endl;
-    }
-    if (!sontAmicaux(1184, 1211)) { // 1211 n'est pas la somme des diviseurs de 1184
-        cout << "1184 et 1211 ne sont pas amicaux." << endl;
-    }
+    int n;
+    cin >> n;
+    cout << somme_impairs(n);
     return 0;
 }
-// --- Ne pas modifier ---
-#include <iostream>
-#include <string>
-#include <cctype>
-using namespace std;
-bool estPalindrome(string& s) {
-    int first = 0;
-    int last = s.size()-1;
-    while(first<last) {
-        if(tolower(static_cast<char>(s[first])) != static_cast<char>(s[last])) {
-            return false;
-        }
-        first++;
-        last--;
-    }
-    return true;
-}
 
-int main() {
-    string mot1 = "Kayak";
-    string mot2 = "bonjour";
-    cout << mot1 << " est un palindrome ? " << boolalpha << estPalindrome(mot1) << endl;
-    cout << mot2 << " est un palindrome ? " << boolalpha << estPalindrome(mot2) << endl;
-    return 0;
-}
-#include <iostream>
-using namespace std;
-int diviser(int a, int b, int & q, int& r) {
-    int aa = a;
-     q=1;
-    while((a-b) >= b ) {
-        a= a-b;
-        ++q;
-    }
-    r = aa - q*b;
-    return q;
-}
-
-int main() {
-    int quotient, reste;
-    diviser(17, 5, quotient, reste);
-    cout << "17 divise par 5 -> Quotient: " << quotient << ", Reste: " << reste << endl; // Attendu: 3, 2
-
-    diviser(8, 4, quotient, reste);
-    cout << "8 divise par 2 -> Quotient: " << quotient << ", Reste: " << reste << endl; // Attendu: 4, 0
-    return 0;
-}
-Défi 4 : Traitement d'un Tableau C-style(tablolar vektorler vs sinav konulari dahilidnde degil.. o yuzden geciyorum bu soruyu)
-
-
-// --- Ne pas modifier ---
-// --- Ne pas modifier ---
-#include <iostream>
-using namespace std;
-void dessinerRectangleCreux(int h, int l) {
-    for(int i =1; i<=l; ++i) {
-        if(i>1 && i<l) {
-            for(int k =1; k<=2; ++k) {
-                cout<<"* ";
-            }
-        }else {
-            for(int j=1; j<=h;++j) {
-            cout<<"*";
-        }
-        }
-        cout<<endl;
-    }
-}
-int main() {
-    cout << "Rectangle 8x5:" << endl;
-    dessinerRectangleCreux(8, 5);
-    return 0;
-}
-// Sortie attendue pour 8x5:
-// ********
-// * *
-// * *
-// * *
-// ********
 
 #include <iostream>
 using namespace std;
 
-int calculerTrajet(int a, int b, int c) {
-   int jour=0;
-    while(a>=0) {
-        a-=b;
-        if(a!=0) {
-            a+=c;
-            ++jour;
-        }
+double moyenne_valide() {
+    int x;
+    long long somme =0;
+    int count = 0;
+
+    while (cin>>x &&  x!= -1) {
+        if(x<0 ||x>6)continue;
+        somme+=x;
+        ++count;
     }
-    return jour;
+    return count == 0 ? 0.0 : static_cast<double>(somme)/count;
 }
+
 
 int main() {
-    // Puits de 10m, monte 3m/jour, descend 2m/nuit
-    int jours1 = calculerTrajet(10, 3, 2);
-    cout << "Il faudra " << jours1 << " jours pour sortir du puits de 10m." << endl; // Attendu: 8 jours
-
-    // Puits de 20m, monte 5m/jour, descend 1m/nuit
-    int jours2 = calculerTrajet(20, 5, 1);
-    cout << "Il faudra " << jours2 << " jours pour sortir du puits de 20m." << endl; // Attendu: 5 jours
+    cout << moyenne_valide();
     return 0;
 }
+
  */
-
-
-// --- Ne pas modifier ---
 #include <iostream>
 using namespace std;
 
+void triangle_creux(int h) {
+    if(h<=0) return ;
+    if(h==1){cout<<"*\n";}
+    cout<<"*\n";
+    for(int i = 2; i<h; ++i) {
+        cout<<"*";
+        for(int s = 0; s<2*i-3; ++s)cout<<" ";
+        cout<<"*\n";
+    }
+    for(int k = 0; k<2*h -3; ++k)
+        cout<<"*";
+    cout<<endl;
 
- int* ptrElementMax( int  *a, int * b) {
-    return (*a > *b ? a : b);
 }
 
 int main() {
-    int x = 50, y = 100;
-    int* ptrMax = nullptr;
-
-    ptrMax = ptrElementMax(&x, &y);
-    cout << "La valeur maximale est : " << *ptrMax << endl; // Attendu: 100
-
-    // Si la fonction retourne un vrai pointeur, cette ligne doit fonctionner
-    // et modifier la variable y à -1.
-    *ptrMax = -1;
-
-    cout << "Apres modification, x=" << x << " et y=" << y << endl; // Attendu: x=50 et y=-1
+    int h;
+    cin >> h;
+    triangle_creux(h);
     return 0;
 }
