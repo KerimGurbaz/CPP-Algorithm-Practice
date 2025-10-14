bfd4221460fccde4d5df2cb472a6bf30db9719c9
Kerim <kerim.grbz.01@gmail.com>
2025-10-14 22:44:14 +0200

    Lecture et ajout de texte dans un fichier

--- main.cpp
+++ main.cpp
@@ -1,37 +1,47 @@
 #include <iostream>
-#include <fstream>
+#include <fstream> // Pour ifstream et ofstream
 #include <string>
+#include <vector>
 using namespace std;
 
 int main() {
- string filename;
-    cout <<" entrez le nom du fichier de sortie..";
-    getline(cin, filename);
 
-    ifstream check_file(filename);
-    if(check_file.is_open()) {
-        cerr<<"Erreur le fichier "<<filename<<"existe deja:"<<endl;
-        check_file.close();
+    string input_filename, output_filename, keyword;
+
+    cout<<"Entrez le nom du fichier d'entree : ";
+    getline(cin, input_filename);
+
+    cout<<"Entrez le nom du fichier sortie : ";
+    getline(cin, output_filename);
+
+    cout<<"Entrez le mot-cle a rechercher : ";
+    getline(cin, keyword);
+
+    ifstream input_file(input_filename);
+    if(!input_file.is_open()) {
+        cerr<<"Erreur : impossible d'ouvrir le fichier d'entree"<<input_filename<<endl;
         return 1;
     }
 
-
-    ofstream output_file(filename);
+    ofstream output_file(output_filename);
     if(!output_file.is_open()) {
-        cerr<<"erreur critique :  impossible de creer de fichier"<< filename<<endl;
+        cerr<<"Erreur imposible de cree le fichier de sortie"<<output_filename<<endl;
+        input_file.close();
+
         return 1;
     }
 
-    cout<<"Entrez le texte a enregistrer..."<<endl;
-    string line;
-    while (getline(cin, line)) {
-        if(line== "#exit#") {
-            break;
+    string ligne;
+    int lines_written =0;
+    cout<<"\nFiltrage en cours..."<<endl;
+    while(getline(input_file, ligne)) {
+        if(ligne.find(keyword) != string::npos) {
+            output_file<<ligne<<endl;
+            lines_written++;
         }
-
-        output_file<<line<<endl;
     }
-cout<<"Saisie est termine.."<<endl;
+
+    input_file.close();
     output_file.close();
 
     return 0;
