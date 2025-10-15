87ce5f0e74750f9bc515336284b67529575c35b9
Kerim <kerim.grbz.01@gmail.com>
2025-10-15 12:28:11 +0200

    Contrôler et tester les saisies, Gestionnaire d'étudiants  v2

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
-#include <sstream>
 #include <limits>
+#include <sstream>
 using namespace std;
-
 
 string lire_string(const string& prompt) {
     string valeur;
@@ -13,44 +12,56 @@ string lire_string(const string& prompt) {
     return valeur;
 }
 
-int lire_int(const string& prompt) {
+int lire_int(const string& prompt, int min, int max) {
     int valeur;
     while(true) {
         cout<<prompt;
         cin>>valeur;
 
         if(cin.fail()) {
-            cout<<"Erreur : saisie nom numerique. veuillez reessayer.\n";
+            cout<<"Erreur: saisie nom numerique...\n";
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        }else {
+            continue;
+        }
+        if(valeur >=min && valeur<=max) {
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
             return valeur;
         }
+
+        cout<<"Erreur: la valeur doit etre entre "<<min<<" et "<<max<<".\n";
     }
+
 }
 
-double lire_double(const string& prompt) {
+double lire_double(const string& prompt, double min, double max) {
+
     double valeur;
     while(true) {
         cout<<prompt;
         cin>>valeur;
-        if(cin.fail() || valeur>6.0 ||valeur<0.0) {
-            cout<<"Erreur: saisie invalide.."<<endl;
+
+        if(cin.fail()) {
+            cout<<"Erreur: saisie non numerique.\n";
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        }else {
+            continue;
+        }
+
+        if(valeur>=min && valeur<=max) {
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
             return valeur;
         }
+        cout << "Erreur: La valeur doit etre entre " << min << " et " << max << ".\n";
     }
 }
+
 double calculer_moyenne(double note1, double note2) {
     return (note1 + note2)/2.0;
 }
 
 bool est_admis(double moyenne) {
-    return moyenne >=3.8;
+    return moyenne >=4.0;
 }
 
 string double_to_str(double valeur) {
@@ -59,16 +70,34 @@ string double_to_str(double valeur) {
     return ss.str();
 }
 
-void afficher(const string& nom, const string& age, const string& note_math, const string& note_prg1, const string& admis) {
-    cout<<left
-    <<setw(18)<<nom
-    <<setw(5)<<age
-    <<setw(14)<<note_math
-    <<setw(13)<<note_prg1
-    <<admis<<endl;
+void afficher_ligne_tableau(const string& nom, const string& age, const string& note_math, const string& note_prg1, const string& admis) {
+    cout<<left<<setw(18)<<nom<<setw(5)<<setw(14)<<note_math<<setw(13)<<note_prg1<<admis<<endl;
 }
 
 void afficher_resume(double moyenne, bool admis) {
     cout<<"Moyenne : "<<fixed<<setprecision(2)<<moyenne<<endl;
-    cout<<"resultat"<<(admis ? "Admis" : "Non Admis")<<endl;
+    cout<<"Resultat : "<<(admis ? "Admis" : "Non Admis")<<endl;
+}
+
+int main() {
+    const int age_min = 1; const int age_max = 100;
+    const double note_min = 1.0; const double note_max = 6.0;
+    const string LIGNE_SEPARATION(59, '-');
+
+    string nom = lire_string("Entrez le nom de l'etudiant.e : ");
+    int age = lire_int("Entrez l'age de l'etudiant.e : ", age_min, age_max);
+    double note_math = lire_double("Entrez la note en mathematiques : ", note_min, note_max);
+    double note_prg1 = lire_double("Entrez la note en PRG1 : ", note_min, note_max);
+
+    double moyenne = calculer_moyenne(note_math, note_prg1);
+    bool admis = est_admis(moyenne);
+
+    cout << endl;
+    afficher_ligne_tableau("Nom", "Age", "Note Math", "Note PRG1", "Admis");
+    cout << LIGNE_SEPARATION << endl;
+    afficher_ligne_tableau(nom, to_string(age), double_to_str(note_math), double_to_str(note_prg1), admis ? "Oui" : "Non");
+    cout << LIGNE_SEPARATION << endl;
+    afficher_resume(moyenne, admis);
+
+    return 0;
 }
