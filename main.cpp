7f72fe27bdbeadefa1142bb2416c2bb327ab78c9
Kerim <kerim.grbz.01@gmail.com>
2025-10-22 15:06:19 +0200

    Jour de la semaine

--- main.cpp
+++ main.cpp
@@ -1,34 +1,59 @@
 
-#include <iostream>;
+#include <iostream>
 
 using namespace std;
 
+enum class Chiffre {
+    ZERO, UN , DEUX, TROIS, QUTRE, CINQ, SIX,SEPT, HUIT, NEUF
+};
+const int nb_chiffres = 10;
 
+void afficher(Chiffre c) {
+    switch(c) {
+        case Chiffre::ZERO:
+            cout<<"ZERO"; break;
+        case Chiffre::UN :
+            cout<<"UN"; break;
+        case Chiffre::DEUX :
+            cout<<"DEUX";break;
+        case Chiffre::TROIS:
+            cout<<"TROIS"; break;
+        case Chiffre::QUTRE:
+            cout<<"QUTRE"; break;
+        case Chiffre::CINQ:
+            cout<<"CINQ"; break;
+        case Chiffre::SIX:
+            cout<<"SIX"; break;
+        case Chiffre::SEPT:
+            cout<<"SEPT"; break;
+        case Chiffre::HUIT:
+            cout<<"HUIT"; break;
+        case Chiffre::NEUF:
+            cout<<"NEUF"; break;
+    }
+}
+
+Chiffre next(Chiffre c) {
+    int val = static_cast<int>(c);
+
+    val = (val +1) % nb_chiffres;
+return static_cast<Chiffre>(val);
+
+}
+
+bool operator<(Chiffre a, Chiffre b) {
+    return static_cast<int>(a) < static_cast<int>(b);
+}
 
 int main() {
+    Chiffre a = Chiffre::NEUF;
+    Chiffre b = next(a);
 
+    afficher(a);
 
-    enum class JOUR {
-        LUNDI, MARDI, MECREDI, JEUDI, VENDREDI, SAMEDI, DIMANCHE
-    };
+cout<< (a<b ? " < " : ">=");
 
-    JOUR jour = JOUR::JEUDI;
-
-    string chaine = "jour de la semaine : ";
-
-    switch(jour) {
-        case JOUR::JEUDI : chaine += "jeudi"; break;
-        case JOUR::LUNDI : chaine += "lundi"; break;
-        case JOUR::MARDI : chaine += "mardi"; break;
-        case JOUR::MECREDI : chaine += "mecredi"; break;
-        case JOUR::VENDREDI : chaine += "vendredi"; break;
-        case JOUR::SAMEDI : chaine += "samedi"; break;
-        case JOUR::DIMANCHE : chaine += "dimanche"; break;
-        default : chaine += "jour inconnu";
-
-    }
-
-    cout<<chaine<<endl;
+    afficher(b);
 
     return 0;
 }
