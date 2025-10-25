7e9c117064418c887c2a3fd52958f512f775592e
Kerim <kerim.grbz.01@gmail.com>
2025-10-25 15:07:59 +0200

    Prénom, nom et acronyme

--- main.cpp
+++ main.cpp
@@ -1,54 +1,30 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include <iomanip>
+#include <utility>
+#include <sstream>
 using namespace std;
-
-int valeur_symbole(char c) {
-    switch(c) {
-        case 'I': return 1;
-        case 'V': return 5;
-        case 'X': return 10;
-        case 'L': return 50;
-        case 'C': return 100;
-        case 'D': return 500;
-        case 'M': return 1000;
-        default : return 0;
+void acronyme(const string& s) {
+    vector<string>v;
+    string mot;
+    string res = "";
+    stringstream ss(s);
+    while(ss>>mot) {
+        v.push_back(mot);
+        res.push_back(mot[0]);
     }
+    res+=toupper(s.back());
+    v.push_back(res);
+    cout<<"Votre prenom est   : "<<v[0]<<endl;
+    cout<<"Votre nom est      : "<<v[1]<<endl;
+    cout<<"Votre acronyme est : "<<v[2]<<endl;
 }
-
-int romain_vers_entier(const string& s) {
-    if(s.empty()) {
-        return 0;
-    }
-    int total =0;
-    int precedent =0;
-    for(int i = s.size()-1; i>=0; --i) {
-        int valuerActuelle = valeur_symbole(s[i]);
-
-        if(valuerActuelle < precedent) {
-            total -= valuerActuelle;
-        }else {
-            total += valuerActuelle;
-        }
-        precedent = valuerActuelle;
-    }
-    return total;
-}
-
-
-
-
 
 int main() {
-    std::vector<std::string> tests = {
-        "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X",
-        "XIV", "MMMMDCCCLXXXVIII"
-    };
-
-    for (const std::string& test : tests) {
-        std::cout << "\"" << test << "\" = " << romain_vers_entier(test) << std::endl;
-    }
-
+    cout<<"Entrez votre prenom et votre nom : ";
+    string nomPrenom;
+    getline(cin, nomPrenom);
+   acronyme(nomPrenom);
     return 0;
 }
