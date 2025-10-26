c20fafb36e6e4a5eebb8ef64ceebf9f7546a711d
Kerim <kerim.grbz.01@gmail.com>
2025-10-26 20:04:32 +0100

    Analyse d'une adresse

--- main.cpp
+++ main.cpp
@@ -7,39 +7,24 @@
 #include <cctype>
 using namespace std;
 
-string to_lower(string& s) {
-    for(char & c: s) {
-       c= tolower(c);
-    }
-    return s;
-}
 
-int occurance( const string& s,const   string& o) {
-    string n_s =s;
-    n_s = to_lower(n_s);
-
-    string n_o = o;
-    n_o = to_lower(n_o);
-
-    int pos=0;
-    int count=0;
-    while((pos = n_s.find(n_o,pos)) != string::npos){
-        pos ++;
-        count++;
-    }
-    return count;
-}
 
 
 int main() {
-    cout<<"Entrez une chaine de caracteres ";
-    string nom;
-    string occur;
-   getline(cin, nom);
-   getline(cin, occur);
+    cout<<"Entrez le nom et le numero de la rue : ";
+    string addresse;
 
-cout<<occurance(nom, occur);
+   getline(cin, addresse);
+    string rue ="";
+    int i = 0;
 
-
+    for(; i<addresse.size()-1; ++i) {
+        if(isalpha(addresse[i]) && isdigit(addresse[i+1])) {
+            rue = addresse.substr(i);
+            break;
+        }
+    }
+    cout<<"Le nom de la rue est :"<<addresse.substr(0, i)<<endl;
+    cout<<"Le no de la rue est  :"<<rue;
     return 0;
 }
