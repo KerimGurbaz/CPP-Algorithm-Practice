12a6d2d4887b6e1b9e9f5f37a41366306a29c68e
Kerim <kerim.grbz.01@gmail.com>
2025-10-26 19:43:34 +0100

    Compter le nombre d'occurrences- case-insensetive

--- main.cpp
+++ main.cpp
@@ -7,41 +7,39 @@
 #include <cctype>
 using namespace std;
 
-bool Palindrome(const string& s) {
-    int first = 0;
-    int last = s.size()-1;
-
-    while(first<last) {
-        while(first<last && !isalnum(s[first])) {
-            first++;
-        }
-        while(first <last && !isalnum(s[last])) {
-            last --;
-        }
-        if(first <last && tolower(s[first]) != tolower(s[last])) {
-            return false;
-        }
-        first++;
-        last--;
+string to_lower(string& s) {
+    for(char & c: s) {
+       c= tolower(c);
     }
-    return true;
+    return s;
 }
 
+int occurance( const string& s,const   string& o) {
+    string n_s =s;
+    n_s = to_lower(n_s);
 
-void affichage (const string& s) {
-    if(Palindrome(s)) {
-        cout<<"Le mot "<<s<<" est un palindrome.";
-    }else {
-          cout<<"Le mot "<<s<<" n'est pas un palindrome..";
+    string n_o = o;
+    n_o = to_lower(n_o);
+
+    int pos=0;
+    int count=0;
+    while((pos = n_s.find(n_o,pos)) != string::npos){
+        pos ++;
+        count++;
     }
-
+    return count;
 }
 
 
 int main() {
-    cout<<"Entrez un nom : ";
+    cout<<"Entrez une chaine de caracteres ";
     string nom;
+    string occur;
    getline(cin, nom);
-   affichage (nom);
+   getline(cin, occur);
+
+cout<<occurance(nom, occur);
+
+
     return 0;
 }
