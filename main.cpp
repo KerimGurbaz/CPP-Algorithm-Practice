b8fdb26b9d1d7e8e58627d3999df31aa752711e4
Kerim <kerim.grbz.01@gmail.com>
2025-10-26 12:19:57 +0100

    Palindrome ou pas ?

--- main.cpp
+++ main.cpp
@@ -4,27 +4,44 @@
 #include <iomanip>
 #include <utility>
 #include <sstream>
+#include <cctype>
 using namespace std;
-void acronyme(const string& s) {
-    vector<string>v;
-    string mot;
-    string res = "";
-    stringstream ss(s);
-    while(ss>>mot) {
-        v.push_back(mot);
-        res.push_back(mot[0]);
+
+bool Palindrome(const string& s) {
+    int first = 0;
+    int last = s.size()-1;
+
+    while(first<last) {
+        while(first<last && !isalnum(s[first])) {
+            first++;
+        }
+        while(first <last && !isalnum(s[last])) {
+            last --;
+        }
+        if(first <last && tolower(s[first]) != tolower(s[last])) {
+            return false;
+        }
+        first++;
+        last--;
     }
-    res+=toupper(s.back());
-    v.push_back(res);
-    cout<<"Votre prenom est   : "<<v[0]<<endl;
-    cout<<"Votre nom est      : "<<v[1]<<endl;
-    cout<<"Votre acronyme est : "<<v[2]<<endl;
+    return true;
 }
 
+
+void affichage (const string& s) {
+    if(Palindrome(s)) {
+        cout<<"Le mot "<<s<<" est un palindrome.";
+    }else {
+          cout<<"Le mot "<<s<<" n'est pas un palindrome..";
+    }
+
+}
+
+
 int main() {
-    cout<<"Entrez votre prenom et votre nom : ";
-    string nomPrenom;
-    getline(cin, nomPrenom);
-   acronyme(nomPrenom);
+    cout<<"Entrez un nom : ";
+    string nom;
+   getline(cin, nom);
+   affichage (nom);
     return 0;
 }
