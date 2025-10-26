2a57905a2503f712732b330ec6a82b0873f4b4f0
Kerim <kerim.grbz.01@gmail.com>
2025-10-26 21:20:07 +0100

    Prenom nom et acronyme 2.md

--- main.cpp
+++ main.cpp
@@ -1,30 +1,52 @@
 #include <iostream>
 #include <string>
-#include <vector>
-#include <iomanip>
-#include <utility>
 #include <sstream>
-#include <cctype>
+#include <vector>
 using namespace std;
+
+void acronyme (const string& s) {
+    vector<string>v;
+    stringstream ss(s);
+    string mot;
+    string acr ="";
+    while(ss>>mot) {
+    v.push_back(mot);
+    }
+    if(v.empty()) {
+        cout<<"Erreur: Aucune entree..";
+        return;
+    }
+
+    string nom="Votre nom est      :";
+    for(int i = 0; i<v.size(); i++) {
+        if(i==0) {
+            cout<<"Votre prenom est   :"<<v[0]<<endl;
+            acr.push_back(toupper(v[i][0]));
+        }else if(i != v.size()) {
+            nom += v[i] + " ";
+            if(i !=v.size()-1) {
+                acr.push_back(toupper(v[i][0]));
+            }
+        }
+    }
+
+    cout<<nom;
+    acr+=toupper(v[v.size()-1].back());
+    cout<<"\nVotre acronyme est :"<<acr;
+    cout<<endl;
+}
 
 
 
 
 int main() {
-    cout<<"Entrez le nom et le numero de la rue : ";
-    string addresse;
+    cout<<"Entrez votre prenom et votre nom : ";
+    string prenomNom;
 
-   getline(cin, addresse);
-    string rue ="";
-    int i = 0;
+   getline(cin, prenomNom);
+    acronyme(prenomNom);
 
-    for(; i<addresse.size()-1; ++i) {
-        if(isalpha(addresse[i]) && isdigit(addresse[i+1])) {
-            rue = addresse.substr(i);
-            break;
-        }
-    }
-    cout<<"Le nom de la rue est :"<<addresse.substr(0, i)<<endl;
-    cout<<"Le no de la rue est  :"<<rue;
+
+
     return 0;
 }
