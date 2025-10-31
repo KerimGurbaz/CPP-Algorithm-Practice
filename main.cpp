1118747b3d99e7a44727916e9fe92ca2183ecee6
KerimGurbaz <kerim.grbz.01@gmail.com>
2025-10-31 11:02:58 +0100

    class - constructeurs et destructeurs

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
-#include <cctype> // ::islower
 using namespace std;
 
+class Compteur {
+public:
+    Compteur() : Compteur(0) { cout << "1"; }
+    Compteur(int i) { cout << "2"; }
+    Compteur(double d) : Compteur(int(d)) { cout << "3"; }
+    ~Compteur() { cout << "0"; }
+};
+
 int main() {
-    string s("-vd-HEIG");
-    remove_if(s.begin(), s.end(), ::islower);
-    cout << s;
+    Compteur* p;
+    {
+        Compteur c1(1);
+        p = new Compteur(2);
+    }
+    cout << "/";
+    delete p;
+
 }
