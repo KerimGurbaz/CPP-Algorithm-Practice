ea0870374918a15d2a549100f3b6b7e628e41e5a
KerimGurbaz <kerim.grbz.01@gmail.com>
2025-10-31 11:32:35 +0100

    transformer_date

--- main.cpp
+++ main.cpp
@@ -1,21 +1,30 @@
+#include <algorithm>
 #include <iostream>
+#include <string>
+#include <vector> // Gerekirse
+#include <sstream> // Gerekirse
+
 using namespace std;
 
-class Compteur {
-public:
-    Compteur() : Compteur(0) { cout << "1"; }
-    Compteur(int i) { cout << "2"; }
-    Compteur(double d) : Compteur(int(d)) { cout << "3"; }
-    ~Compteur() { cout << "0"; }
-};
+string transformer_date(const string& s) {
 
-int main() {
-    Compteur* p;
-    {
-        Compteur c1(1);
-        p = new Compteur(2);
+string result="";
+    int pos1 = s.find_first_of('-');
+    int pos2 = s.find_last_of('-');
+    string day = s.substr(pos2 +1, 2);
+    string mounth = s.substr(pos1+1,2);
+    string year = s.substr(0, 4);
+    vector<string> ss = {day, mounth, year};
+    for(int i = 0; i<ss.size(); ++i) {
+        result += ss[i];
+        if(i != ss.size()-1) {
+            result += ".";
+        }
     }
-    cout << "/";
-    delete p;
+    return result;
 
+}
+int main() {
+    cout << transformer_date("2025-10-31") << endl;
+    cout << transformer_date("1999-01-20") << endl;
 }
