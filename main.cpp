47182f132536d777ccc43e5c7c9c49dc211b0ca0
KerimGurbaz <kerim.grbz.01@gmail.com>
2025-10-31 13:22:21 +0100

    transformer_date, compress et palindrome

--- main.cpp
+++ main.cpp
@@ -1,30 +1,100 @@
-#include <algorithm>
-#include <iostream>
-#include <string>
-#include <vector> // Gerekirse
-#include <sstream> // Gerekirse
+/*
+* #include <iostream>
+#include <vector>
+#include <array>
 
 using namespace std;
 
-string transformer_date(const string& s) {
+size_t oter_hors_limite(vector<double>&v , array<double, 2>limites) {
+    double min = limites[0];
+    double max = limites[1];
 
-string result="";
-    int pos1 = s.find_first_of('-');
-    int pos2 = s.find_last_of('-');
-    string day = s.substr(pos2 +1, 2);
-    string mounth = s.substr(pos1+1,2);
-    string year = s.substr(0, 4);
-    vector<string> ss = {day, mounth, year};
-    for(int i = 0; i<ss.size(); ++i) {
-        result += ss[i];
-        if(i != ss.size()-1) {
-            result += ".";
+    size_t slow = 0;
+    for(size_t fast = 0; fast<v.size();++fast) {
+        if((v[fast] >= min) && (v[fast] <= max)) {
+                v[slow]= v[fast];
+                ++slow;
+        }
+    }
+    return slow;
+}
+
+
+int main() {
+    vector<double> v1{1.0, 2.3, 1.4, 5.3, 3.4, 2.4, 4.6};
+    array<double, 2> lim1{2.3, 5.0};
+
+    // v1 = {1.0, 2.3, 1.4, 5.3, 3.4, 2.4, 4.6}
+    // Korunanlar: 2.3, 3.4, 2.4, 4.6 (5.3 aralık dışında)
+
+    size_t n1 = oter_hors_limite(v1, lim1);
+
+    // Sadece yeni boyuta kadar bas
+    for (size_t i = 0; i < n1; ++i)
+        cout << v1[i] << ' ';
+    // Beklenen Çıktı: 2.3 3.4 2.4 4.6
+
+    cout << "\nvektorun son hali: ";
+    for (double e : v1) cout << e << ' ';
+
+}
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+bool est_palindrome_insensible(const string& s) {
+    int first = 0;
+    int last = s.size()-1;
+
+    while(first < last) {
+        while(ispunct(s[first]) || isspace(s[first])) {
+            ++first;
+        }
+        while(ispunct(s[last]) || isspace(s[last])) {
+            --last;
+        }
+        if(tolower(s[first]) !=tolower( s[last])) {
+            return false;
+        }
+        ++first;
+        --last;
+    }
+    return true;
+}
+
+int main() {
+    cout << boolalpha; // 'true'/'false' basar
+    cout << est_palindrome_insensible("Radar") << endl;
+    cout << est_palindrome_insensible("A man, a plan, a canal: Panama") << endl;
+    cout << est_palindrome_insensible("hello") << endl;
+}
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+string compresser(const string& s) {
+    int count =1;
+
+
+    string result ="";
+    for(size_t i = 0; i<s.size();++i) {
+        if(s[i]==s[i+1]) {
+            count++;
+        }else if(s[i] != s[i+1]) {
+
+            result +=s[i];
+            result += to_string(count) ;
+            count=1;
         }
     }
     return result;
-
 }
+
 int main() {
-    cout << transformer_date("2025-10-31") << endl;
-    cout << transformer_date("1999-01-20") << endl;
+    cout << compresser("aabcccccaaa") << endl;
+   cout << compresser("WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB") << endl;
+    cout << compresser("abc") << endl;
 }
