abccdf5e6c857b45c92b2d8f8ea4b5b9ed0ed8d8
KerimGurbaz <kerim.grbz.01@gmail.com>
2025-11-02 23:33:34 +0100

    fusion_sorted et verifier_tri v2

--- main.cpp
+++ main.cpp
@@ -1,69 +1,97 @@
-/*
-* #include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
-
-bool verifier_tri(const vector<int>& v) {
- for(size_t i = 1 ; i<v.size();++i) {
-     if(v[i]<v[i-1]) {
-         return false;
-     }
- }
-    return true;
-}
-
-int main() {
-    cout << boolalpha;
-    cout << verifier_tri({1,2,3,3,5}) << endl;   // true
-    cout << verifier_tri({1,3,2,5}) << endl;     // false
-}
-
- */
-
 #include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
-vector<int>fusion_sorted(const vector<int>&v1,
-                        const vector<int>&v2) {
-    vector<int>r;
-    r.reserve((v1.size()+ v2.size()));
-    int p1 =0 ;
-    int p2 = 0;
-
-    while(p1 < v1.size() && p2 < v2.size() ) {
-        if(v1[p1] < v2[p2] ){
-            r.push_back(v1[p1]);
-            p1++;
+// TODO 1: tri_bulle - klasik bubble sort (erken çıkış optimizasyonu serbest)
+// İmza sabit: in-place, artan sıraya göre sıralasın.
+void tri_bulle(vector<int> &v) {
+if(v.size()<2)return;
+    for(size_t i = 0; i<v.size()-1; ++i) {
+        size_t minIdx = i;
+        for(size_t j =i+1; j<v.size(); ++j) {
+            if(v[j] < v[minIdx]) {
+                minIdx = j;
+            }
+            if(minIdx != i) {
+                swap(v[i], v[minIdx]);
+            }
         }
-       else if(v2[p2]< v1[p1] ){
-            r.push_back(v2[p2]);
-            p2++;
-        }else {
-            r.push_back(v2[p2]);
-            r.push_back(v1[p1]);
-            p1++;
-            p2++;
+    }
+
+}
+
+// TODO 2: tri_selection - min-seçimli sıralama (durağan/stable olmak zorunda değil)
+void tri_selection(vector<int> &v) {
+    for(int i = 0; i<v.size()-1; ++i) {
+         size_t min_idx =i;
+        for(int j = i+1; j<v.size(); ++j) {
+            if(v[j] < v[min_idx]) {
+                min_idx = j;
+            }
         }
-
-    }
-    while(p1 < v1.size()) {
-        r.push_back(v1[p1]);
-        ++p1;
-    }
-    while(p2 < v2.size()) {
-        r.push_back(v2[p2]);
-        ++p2;
+        int temp = v[min_idx];
+        v[min_idx] = v[i];
+        v[i]= temp;
     }
 
-    return r;
+}
+
+// TODO 3: tri_insertion - eklemeli sıralama (stabledır)
+// v[i]’yi soldaki uygun yerine kaydırarak yerleştir.
+void tri_insertion(vector<int> &v) {
+   for(int i = 0; i<v.size()-1; ++i) {
+       for(int j = i; i<v.size() -i; ++j) {
+           if(v[j] < v[i]) {
+               int temp = v[j];
+               v[j]= v[i];
+               v[i]=temp;
+           }
+       }
+   }
+}
+
+// (isteğe bağlı kullanırsın) küçük yardımcı
+static void printv(const vector<int> &v, const char *tag) {
+    cout << tag << " : ";
+    for (int x: v) cout << x << ' ';
+    cout << '\n';
 }
 
 int main() {
-    vector<int> a{1,3,5,9};
-    vector<int> b{2,4,6,7,8};
-    auto r = fusion_sorted(a,b);
-    for (int x : r) cout << x << ' ';
+    vector<int> A{5, 1, 4, 2, 8};
+    vector<int> B{3, 3, 1, 0, -2, 3, 1};
+    vector<int> C{9, 7, 5, 3, 1, -1};
+    vector<int> D{1, 2, 3, 4, 5}; // zaten sıralı
+    vector<int> E{2}; // tek eleman
+    vector<int> F{}; // boş
+
+    // Bubble
+    auto v1 = A;
+    tri_bulle(v1);
+    printv(v1, "bubble(A)");
+    auto v2 = B;
+    tri_bulle(v2);
+    printv(v2, "bubble(B)");
+
+    // Selection
+    auto v3 = C;
+    tri_selection(v3);
+    printv(v3, "select(C)");
+    auto v4 = D;
+    tri_selection(v4);
+    printv(v4, "select(D)");
+
+    // Insertion
+    auto v5 = A;
+    tri_insertion(v5);
+    printv(v5, "insert(A)");
+    auto v6 = F;
+    tri_insertion(v6);
+    printv(v6, "insert(F)");
+
+    // (İstersen) burada is_sorted ile doğruluk testi yapabilirsin
+    // cout << boolalpha << is_sorted(v1.begin(), v1.end()) << '\n';
+
+    return 0;
 }
