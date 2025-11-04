e68fea8b59baf9b1c0c2c166cb8b0094458bbbba
KerimGurbaz <kerim.grbz.01@gmail.com>
2025-11-04 21:41:28 +0100

    stuck

--- cmake-build-debug/CMakeFiles/untitled20.dir/main.cpp.obj.d
+++ cmake-build-debug/CMakeFiles/untitled20.dir/main.cpp.obj.d
@@ -150,15 +150,19 @@ CMakeFiles/untitled20.dir/main.cpp.obj: \
  C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/bits/ostream.tcc \
  C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/istream \
  C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/bits/istream.tcc \
- C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/iomanip \
- C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/locale \
- C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/bits/locale_facets_nonio.h \
- C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/ctime \
- C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/x86_64-w64-mingw32/bits/time_members.h \
- C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/x86_64-w64-mingw32/bits/messages_members.h \
- C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/bits/codecvt.h \
- C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/bits/locale_facets_nonio.tcc \
- C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/bits/locale_conv.h \
- C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/bits/quoted_string.h \
- C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/sstream \
- C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/bits/sstream.tcc
+ C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/array \
+ C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/algorithm \
+ C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/bits/stl_algo.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/bits/algorithmfwd.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/bits/stl_heap.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/bits/uniform_int_dist.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/bits/stl_tempbuf.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/bits/ranges_algo.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/bits/ranges_algobase.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/pstl/glue_algorithm_defs.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/pstl/execution_defs.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/vector \
+ C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/bits/stl_uninitialized.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/bits/stl_vector.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/bits/stl_bvector.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/bits/vector.tcc
--- main.cpp
+++ main.cpp
@@ -1,45 +1,82 @@
 #include <iostream>
-#include <string>
+#include <array>
+#include <algorithm>
+#include <vector>
 using namespace std;
-#include <iomanip>
+const int CAPACITY = 10;
 
-enum class StatutLivre{DISPONIBLE=0, EMPRUNTE, RESERVE};
-struct Livre {
-    string titre;
-    string auteur;
-    int anne;
-    StatutLivre statut;
+struct Stack {
+    array<int, CAPACITY> a;
+    size_t sz = 0;
+    const int maxVal = 10;
 };
 
-void afficher_livre(const Livre& l) {
-    cout<<left;
+void display(const Stack &s) {
+    cout << "size : " << s.sz;cout<<endl;
+    cout << "data : " ;
 
-    cout<<setw(8)<<"Titre  : "<<l.titre<<endl;
-    cout<<setw(8)<<"Auteur : "<<l.auteur<<endl;
-    cout<<setw(8)<<"Annee  : "<<l.anne<<endl;
-    cout<<setw(8)<<"Statut : ";
 
-        switch(l.statut) {
-            case StatutLivre::DISPONIBLE : cout<<"DISPONIBLE";break;
-            case StatutLivre::EMPRUNTE : cout<<"EMPRUNTE";break;
-            case StatutLivre::RESERVE : cout<<"RESERVE";break;
-
-            default:cout <<"inconnu";break;
+    cout << "[";
+    for (size_t i = 0; i < s.sz; ++i) {
+        cout << s.a[i];
+        if (i < s.sz -1) {
+            cout << ",";
+        }
     }
-    cout<<endl;
-    cout<<endl;
-
-}
-void changer_statut(Livre& l, StatutLivre nouveau) {
- l.statut = nouveau;
-
+    cout << "]" << endl;
 }
 
+bool full(const Stack &s) {
+    return s.sz == CAPACITY;
+}
 
+bool empty(const Stack &s) {
+    return s.sz == 0;
+}
+
+void push(Stack &s, const int &n) {
+    if (!full(s)) {
+        s.a[s.sz] = n;
+        s.sz++;
+    }
+}
+
+int top(const Stack &s) {
+    return s.a[s.sz - 1];
+}
+
+void pop(Stack &s) {
+    if(!empty(s)) {
+            s.sz--;
+    }
+}
+
+int size(Stack &s) {
+    return s.sz;
+}
 
 int main() {
-    Livre l1{"Cien Años de Soledad", "Gabriel García Márquez", 1967, StatutLivre::DISPONIBLE};
-    afficher_livre(l1);
-    changer_statut(l1, StatutLivre::EMPRUNTE);
-    afficher_livre(l1);
+    Stack s;
+    int i = 1;
+
+    display(s);
+    cout << endl;
+
+    while (not full(s)) {
+        push(s, i *= 2);
+    }
+
+    cout << "top  : " << top(s) << endl;
+    cout << "size : " << size(s) << endl;
+    cout << endl;
+
+    display(s);
+    cout << endl;
+
+    while (not empty(s)) {
+        pop(s);
+    }
+
+    display(s);
+    cout << endl;
 }
