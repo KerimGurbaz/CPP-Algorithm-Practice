1dd31a9653e29fa7540f9e44a32d311d95eb0151
KerimGurbaz <kerim.grbz.01@gmail.com>
2025-11-06 13:49:00 +0100

    struct - Date et Personne

--- main.cpp
+++ main.cpp
@@ -1,43 +1,43 @@
 #include <iostream>
-#include <array>
-#include <algorithm>
-#include <vector>
 using namespace std;
+#include <iomanip>
 
-void affichage(const vector<int> &v) {
-    for (int c: v) {
-        cout << c << " ";
-    }
-    cout << endl;
-}
-
-void my_push_back(vector<int> &v, const int n) {
-    v.push_back(n);
-    affichage(v);
+struct Date {
+    int day;
+    int mois;
+    int annee;
 };
 
-void my_pop_back(vector<int> &v) {
-    v.pop_back();
-    affichage(v);
+struct Personne {
+    string nom;
+    string ville;
+    Date date;
 };
 
-void my_resize(vector<int> &v, const int n) {
-    v.resize(n);
-    affichage(v);
+void afficher_date(Date &d) {
+    cout << d.day << "."
+            << d.mois << "."
+            << d.annee << "\n";
 }
 
-void my_resize(vector<int> &v, const int n, const int k) {
-    v.resize(n, k);
-    affichage(v);
+void afficher_personne(Personne &p) {
+    cout << "Nom    : " << p.nom << "\n";
+    cout << "Adresse    : "<< p.ville << "\n";
+    cout << "Anniversaire   : "; afficher_date(p.date);
 }
 
 
 int main() {
-    vector<int> v{1, 2, 3, 4, 5, 6};
-    my_push_back(v, 7); // v contient {1, 2, 3, 4, 5, 6, 7}
-    my_pop_back(v); // v contient {1, 2, 3, 4, 5, 6}
-    my_pop_back(v); // v contient {1, 2, 3, 4, 5}
-    my_resize(v, 3); // v contient {1, 2, 3}
-    my_resize(v, 5, 1); // v contient {1, 2, 3, 1, 1}
-    my_resize(v, 8); // v contient {1, 2, 3, 1, 1, 0, 0, 0}
+    Date uneDate = {30, 10, 2023};
+    cout << "une date     : ";
+    afficher_date(uneDate);
+    cout << endl << endl;
+
+    Personne anna = {"Anna", "Yverdon", uneDate};
+    afficher_personne(anna);
+
+    cout << endl;
+
+
+    return 0;
 }
