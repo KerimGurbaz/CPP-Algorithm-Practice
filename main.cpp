288dc2b7842557db18fd3109bd28d90c7188eae9
KerimGurbaz <kerim.grbz.01@gmail.com>
2025-12-07 20:05:12 +0100

    class Stack

--- main.cpp
+++ main.cpp
@@ -243,3 +243,214 @@ using namespace std;
 ////     // attendu: 1.0 + 2.5 + 3.5 = 7.0
 //// }   it = next_it;
 //
+//Exercice 3 — total_size & concat (version simple)
+// #include <vector>
+// #include <utility>
+// #include <iostream>
+// #include <algorithm>
+// using namespace std;
+//
+// template <typename T> using Iterateur = typename vector<T>::const_iterator;
+// template <typename T> using Plage     = pair<Iterateur<T>, Iterateur<T>>;
+// template <typename T> using vPlages   = vector<Plage<T>>;
+//
+// template<typename InputIterator>
+// size_t total_size(InputIterator first, InputIterator last) {
+//     size_t sum = 0;
+//
+//     for(auto it = first; it != last; ++it) {
+//         sum += distance(it->first, it->second);
+//     }
+//     return sum;
+// }
+//
+//
+// template<typename InputIterator, typename OutputIterator>
+// OutputIterator concat(InputIterator first, InputIterator last, OutputIterator result) {
+//     for(auto it =first; it!= last; ++it) {
+//         //result = copy(it->first, it->second, result);
+//         auto sub_it = it->first;
+//         auto sub_end =it-> second;
+//
+//         while(sub_end != sub_it) {
+//             *result = *sub_it;
+//             result++;
+//             sub_it++;
+//         }
+//     }
+//     return result;
+// }
+//
+//
+// int main() {
+//     const vector<int> v1 {11, 12, 13, 14, 15, 16};
+//     const vector<int> v2 {21, 22, 23, 24};
+//     vector<int>       v3 {31, 32};
+//
+//     const vPlages<int> plages {
+//             {v1.cbegin()+3, v1.cend()},   // 14,15,16
+//             {v1.cbegin(),   v1.cend()-3}, // 11,12,13
+//             {v2.cbegin(),   v2.cend()},   // 21,22,23,24
+//             {v3.cbegin(),   v3.cend()}    // 31,32
+//     };
+//
+//     size_t n = total_size(plages.begin(), plages.end());
+//     cout << "total_size = " << n << '\n';
+//
+//     vector<int> v4(n);
+//     concat(plages.cbegin(), plages.cend(), v4.begin());
+//
+//     cout << "v4 : ";
+//     for (int x : v4) cout << x << ' ';
+//     cout << '\n';
+// }
+
+
+// Exercice 4 — supprimer_si
+
+
+// template<typename T, typename Predicate>
+// void supprimer_si(vector<T>& v, Predicate pred) {
+//     size_t write = 0;
+// for(size_t read =0; read<v.size();++read) {
+//     if(pred(v[read])) {
+//         v[write++] = v[read];
+//     }
+// }
+//     v.resize(write);
+//
+// }
+
+// template<typename T, typename Predicate>
+// void supprimer_si(vector<T>& v, Predicate pred) {
+//     auto write = v.begin();
+//     for(auto read = v.begin(); read != v.end(); ++read ) {
+//         if(!pred(*read)) {
+//             *write++ =*read;
+//         }
+//     }
+//     v.erase(write, v.end());
+// }
+// bool est_pair(int a) { return a % 2 == 0; }
+//
+// int main() {
+//     string s = "C++17 is awesome! 1234";
+//
+//     // string'i vector<char>'a kopyala
+//     vector<char> v(s.begin(), s.end());
+//
+//     // tüm digit'leri sil
+//     supprimer_si(v, ::isdigit);
+//
+//     // yeniden string'e dönüştür
+//     string result(v.begin(), v.end());
+//
+//     cout << result << endl;
+// }
+
+// #include <iostream>
+// using namespace std;
+//
+// template<typename T, typename U>
+// void h(T, U)      { cout << "1"; }   // genel
+//
+// template<typename T, typename U>
+// void h(T*, U*)    { cout << "2"; }   // pointer versiyonu
+//
+// template<typename T>
+// void h(T, T)      { cout << "3"; }   // iki parametre aynı tip
+// template<typename T>
+// void h(T*, T*)      { cout << "4"; }
+//
+//
+// int main() {
+//   int i = 5;
+//   int* ptr = &i;
+//   h(ptr, ptr);
+//
+// }
+
+#include <iostream>
+#include <vector>
+#include <array>
+using namespace std;
+
+// template<typename T, size_t N = 100>
+//
+// class Stack {
+// private:
+//     array<T, N> data;
+//     size_t currentSize;
+//
+// public:
+//     Stack() : currentSize(0) {
+//     }
+//
+//     bool full() const {
+//         return currentSize == N;
+//     }
+//
+//     bool empty() const {
+//         return currentSize == 0;
+//     }
+//
+//     size_t size() const {
+//         return currentSize;
+//     }
+//
+//     void push(const T &el) {
+//         if (!full()) {
+//             data[currentSize] = el;
+//             currentSize++;
+//         }
+//     }
+//
+//     void pop() {
+//         if (!empty()) {
+//             currentSize--;
+//         }
+//     }
+//
+//     const T &top() const {
+//         return data[currentSize - 1];
+//     }
+//
+//     void display() const {
+//         cout << "size : " << currentSize << endl;
+//         cout << "data : [";
+//         for (size_t i = 0; i < currentSize; ++i) {
+//             if (i)cout << ",";
+//             cout << data[i];
+//         }
+//         cout << "]" << endl;
+//     }
+// };
+//
+// int main() {
+//     // Stack d'entiers avec capacité 10
+//     Stack<int, 10> s;
+//     int i = 1;
+//
+//     s.display();
+//     cout << endl;
+//
+//     while (not s.full()) {
+//         s.push(i *= 2);
+//     }
+//
+//     cout << "top  : " << s.top() << endl;
+//     cout << "size : " << s.size() << endl;
+//     cout << endl;
+//
+//     s.display();
+//     cout << endl;
+//
+//     while (not s.empty()) {
+//         s.pop();
+//     }
+//
+//     s.display();
+//     cout << endl;
+//
+//     return 0;
+// }
