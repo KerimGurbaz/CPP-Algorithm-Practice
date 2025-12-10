764da656de4cdcc2b6fac1229e13f95df309dc61
KerimGurbaz <kerim.grbz.01@gmail.com>
2025-12-10 21:07:37 +0100

    HeureMinutes v2

--- cmake-build-debug/CMakeFiles/untitled20.dir/main.cpp.obj.d
+++ cmake-build-debug/CMakeFiles/untitled20.dir/main.cpp.obj.d
@@ -154,4 +154,10 @@ CMakeFiles/untitled20.dir/main.cpp.obj: \
  C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/bits/stl_uninitialized.h \
  C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/bits/stl_vector.h \
  C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/bits/stl_bvector.h \
- C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/bits/vector.tcc
+ C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/bits/vector.tcc \
+ C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/x86_64-w64-mingw32/include/complex.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/list \
+ C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/bits/stl_list.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/bits/allocated_ptr.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/ext/aligned_buffer.h \
+ C:/Program\ Files/JetBrains/CLion\ 2024.1.4/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include/c++/bits/list.tcc
--- main.cpp
+++ main.cpp
@@ -1,291 +1,112 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
+// #ifndef VOITURE_H
+// #define VOITURE_H
 //
-// template<typename T>
-// class Point {
+// class Voiture {
 // private:
-//     T x, y;
-//     T xMax, yMax;
+//     double capacite;
+//     double niveau;
+//     double conso;
+//     static double prix;
 //
 // public:
-//     Point(T xVal = T{}, T yVal = T{}, T xMaxVal = {100}, T yMaxVal = {100}) : x(xVal), y(yVal), xMax(xMaxVal), yMax(yMaxVal) {}
+//     Voiture(double cap, double cons);
 //
-//     void afficher() {
-//         cout << "(" << x << ", " << y << "), contraintes: [0," << xMax << "]X[0," << yMax << "]\n";
-//     }
+//     double effectuerTrajet(double distanceKm);
 //
-//     void deplacer(T dx, T dy) {
-//         x += dx;
-//         y += dy;
-//     }
+//     static void setPrixEssence(double nouveauEssence);
 //
-//     void setX(T xVal) {
-//         x = xVal;
-//     }
-//
-//     void setY(T yVal) {
-//         y = yVal;
-//     }
-//
-//     T getX() const {
-//         return x;
-//     }
-//
-//     T getY() const {
-//         return y;
-//     }
+//     static double getPrixEssence();
 // };
 //
-//
-// int main() {
-//     Point p1(1.2, 2.4);
-//     cout << "p1";
-//     p1.afficher();
-//     p1.deplacer(1., 3.);
-//     cout << "p1";
-//     p1.afficher();
-//     p1.setX(4.);
-//     cout << "p1";
-//     p1.afficher();
-//     p1.setY(7.);
-//     cout << "p1";
-//     p1.afficher();
-//
-//     Point p2(3., 4.2, 10., 10.);
-//     cout << "p2";
-//     p2.afficher();
-//     p2.deplacer(15, 2);
-//     cout << "p2";
-//     p2.afficher();
-//     p2.deplacer(3, 2);
-//     cout << "p2";
-//     p2.afficher();
-//     p2.setX(-3);
-//     cout << "p2";
-//     p2.afficher();
-//     p2.setY(12);
-//     cout << "p2";
-//     p2.afficher();
-// }
-
-// template<typename T>
-// class Pays {
-// private:
-//     string nom;
-//     T habitant;
-//     T superficie;
-// public:
-//
-//
-//
-//
-// };
-//
-//
-// int main() {
-//     vector<Pays> v_pays{
-//         Pays("Suisse", 8.121830, 41290),
-//         Pays("France", 66.663766, 547030),
-//         Pays("Italie", 61.855120, 301230),
-//         Pays("Allemagne", 80.854408, 357021)
-// };
-//
-//     size_t idx_max_superficie = 0;
-//     size_t idx_max_population = 0;
-//     size_t idx_max_densite = 0;
-//
-//     for (size_t i = 0; i < v_pays.size(); ++i){
-//         if (v_pays[i].get_superficie() > v_pays[idx_max_superficie].get_superficie()) {
-//             idx_max_superficie = i;
-//         }
-//         if (v_pays[i].get_population() > v_pays[idx_max_population].get_population()) {
-//             idx_max_population = i;
-//         }
-//         if (v_pays[i].calcule_densite_population() > v_pays[idx_max_densite].calcule_densite_population()) {
-//             idx_max_densite = i;
-//         }
-//     }
-//     cout << "Pays ayant la plus grande superficie               : "
-//          << v_pays[idx_max_superficie].get_nom()
-//          << endl
-//          << "Pays ayant le plus d'habitants                     : "
-//          << v_pays[idx_max_population].get_nom()
-//          << endl
-//          << "Pays ayant la densite de population la plus grande : "
-//          << v_pays[idx_max_densite].get_nom()
-//          << endl;
-// }
-
-
-// template<typename T>
-// class Point {
-// private:
-//     T x, y;
-//
-//
-// public:
-//     Point():x(0.0), y(0.0){}
-//     Point(T xVal = T{}, T yVal=T{}  ): x(xVal), y(yVal) {}
-//
-//     void afficher() {
-//         cout << "(" << x << ", " << y<<")";
-//     }
-//
-//     void deplacer(T dx, T dy) {
-//         x += dx;
-//         y += dy;
-//     }
-//
-//     void setX(T xVal) {
-//         x = xVal;
-//     }
-//
-//     void setY(T yVal) {
-//         y = yVal;
-//     }
-//
-//     T getX() const {
-//         return x;
-//     }
-//
-//     T getY() const {
-//         return y;
-//     }
-// };
-//
-//
-//
-//
-// int main() {
-//     Point centre;
-//     centre.afficher();
-//
-//     Point p(1.2,2.4);
-//     p.afficher();
-//
-//     p.deplacer(0.8, 0.6);
-//     p.afficher();
-//
-//     p.setX(5.5);
-//     p.setY(10.3);
-//     cout << p.getX() << " -- " << p.getY() << endl;
-// }
-
-// template <typename T>
-// class MonEntier {
-// private:
-//     T n;
-// public:
-//     MonEntier():MonEntier(n(0)){}
-//     MonEntier(T nVal = T(0)) : n(nVal){}
-//
-//     MonEntier operator ++(int) {
-//         T temp = *this;
-//         n +=1;
-//         return temp;
-//     }
-//
-//     friend ostream& operator<<(ostream& os, const MonEntier& m) {
-//         os<<m;
-//         return os;
-//     }
-//
-//     MonEntier operator + (const MonEntier& lhs, const MonEntier& rhs) {
-//         return (lhs.n + rhs.n);
-//     }
-//     MonEntier& operator +=( MonEntier lhs, const MonEntier& rhs) {
-//         return (lhs.n + rhs.n);
-//     }
-//     bool operator==(const MonEntier& lhs, const MonEntier& rhs) {
-//         return lhs.n == rhs.n;
-//     }
-//
-//
-// };
-//
-//
-// int main() {
-//     MonEntier m0, m1(1), m2 = 5;
-//     const MonEntier m3{7};
-//
-//     cout << m0 << endl
-//          << m1 << endl;
-//
-//     cout << boolalpha;
-//     cout << (m0 == m1) << endl;
-//     cout << (m0 != m1) << endl;
-//     cout << noboolalpha;
-//
-//     cout << m1++ << endl;
-//     cout << m1 << endl;
-//     cout << ++m1 << endl;
-//
-//     cout << m1 + m2 << endl;
-//     m1 += m2;
-//     cout << m1 << endl;
-//     cout << m3 + m2 << endl;
-// }
-
-// template<typename T>
-// void yazdir(const T& valeur) {
-//     cout<<valeur;
-// }
-//
-// template<typename T>
-// void yazdir( T* ptr) {
-//     if(ptr ==nullptr) {
-//         cout<<"vide";
-//     }
-//     else{cout<<*ptr;}
-// }
-//
-// int main() {
-//     int x = 42;
-//     string s = "Hello";
-//     double* d_ptr = new double(3.14);
-//     int* i_ptr = nullptr;
-//
-//     yazdir(x);
-//     cout << " | ";
-//     yazdir(s);
-//     cout << " | ";
-//
-//
-//     yazdir(d_ptr);
-//     cout << " | ";
-//
-//
-//     yazdir(i_ptr);
-//
-//     delete d_ptr;
-//     return 0;
-// }
-//
-// #ifndef COMPTEUR_H
-// #define COMPTEUR_H
-//
-// class Compteur {
-// private:
-//     int x;
-// public:
-//     Compteur(int xVal=0);
-//     void incrementer(int d=1);
-//
-//     int getVal()const;
-//
-// };
 // #endif
 //
-// #include "Compteur.h"
+// // Voiture.cpp
 // #include <iostream>
-// Compteur::Compteur(int xVal):x(xVal){};
-// int Compteur::getVal()const {
-//     return x;
+// #include "Voiture.h"
+//
+// double Voiture::prix =0.0;
+// Voiture::Voiture(double cap, double cons) : capacite(cap), conso(cons) {
+//     niveau(capacite);
 // }
 //
-// void Compteur::incrementer(int d) {
-//     x+=d;
+// double Voiture::effectuerTrajet(double disKm) {
+//     double LitreUtilise = (disKm * conso) / 100.0;
+//     niveau -= LitreUtilise;
+//     while(niveau <0) {
+//         niveau += capacite;
+//     }
+//
+//     return LitreUtilise * prix;
+// }
+//
+// void Voiture::setPrixEssence(double nouveauEssence) {
+//     prix = nouveauEssence;
+// }
+//
+// double Voiture::getPrixEssence() {
+//     return prix;
 // }
 
+
+// HeureMinute.h
+#include <bits/ranges_algo.h>
+#ifndef HEUREMINUTE_H
+#define HEUREMINUTE_H
+#include <iostream>
+#include <cstdint> // int8_t için
+
+struct HeureMinute {
+    int8_t heure;
+    int8_t minute;
+
+    friend std::ostream &operator<<(std::ostream &os, const HeureMinute &hm);
+
+    friend std::istream &operator>>(std::istream &is, HeureMinute &hm);
+
+    friend bool operator<(const HeureMinute &lhs, const HeureMinute &rhs);
+
+    friend HeureMinute operator+(const HeureMinute &lhs, const HeureMinute &rhs);
+
+    // Prototip ekle: operator<<, operator>>, operator<, operator+
+};
+#endif
+
+#include "HeureMinutes.h"
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+ostream &operator<<(std::ostream &os, const HeureMinute &hm) {
+    os << setw(2) << setfill('0') << +hm.heure << ":"
+            << setw(2) << setfill('0') << +hm.minute;
+    return os;
+}
+
+istream &operator>>(std::istream &is, HeureMinute &hm) {
+    int h, m;
+    char sep;
+
+    if (is >> h >> sep >> m) {
+        hm.heure = static_cast<int8_t>(h);
+        hm.minute = static_cast<int8_t>(m);
+    }
+    return is;
+}
+
+bool operator<(const HeureMinute &lhs, const HeureMinute &rhs) {
+    if (lhs.heure != rhs.heure) {
+        return lhs.heure < rhs.heure;
+    } else {
+        return lhs.minute < rhs.minute;
+    }
+}
+
+
+
+HeureMinute operator+(const HeureMinute &lhs, const HeureMinute &rhs) {
+int totalMinutes = (lhs.heure*60 + lhs.minute) + rhs.heure*60 +rhs.minute;
+    totalMinutes = totalMinutes %1440;
+
+    return HeureMinute{static_cast<int8_t>(totalMinutes /60),
+    static_cast<int8_t>(totalMinutes%60)};
+}
