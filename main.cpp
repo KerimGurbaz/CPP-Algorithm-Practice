fd39cd0c7a4963a71dedc2fa9cf8ae48484ebd2a
KerimGurbaz <kerim.grbz.01@gmail.com>
2025-12-10 22:09:00 +0100

    class Pays avec algorithm max_element

--- main.cpp
+++ main.cpp
@@ -1,112 +1,76 @@
-// #ifndef VOITURE_H
-// #define VOITURE_H
-//
-// class Voiture {
-// private:
-//     double capacite;
-//     double niveau;
-//     double conso;
-//     static double prix;
-//
-// public:
-//     Voiture(double cap, double cons);
-//
-//     double effectuerTrajet(double distanceKm);
-//
-//     static void setPrixEssence(double nouveauEssence);
-//
-//     static double getPrixEssence();
-// };
-//
-// #endif
-//
-// // Voiture.cpp
-// #include <iostream>
-// #include "Voiture.h"
-//
-// double Voiture::prix =0.0;
-// Voiture::Voiture(double cap, double cons) : capacite(cap), conso(cons) {
-//     niveau(capacite);
-// }
-//
-// double Voiture::effectuerTrajet(double disKm) {
-//     double LitreUtilise = (disKm * conso) / 100.0;
-//     niveau -= LitreUtilise;
-//     while(niveau <0) {
-//         niveau += capacite;
-//     }
-//
-//     return LitreUtilise * prix;
-// }
-//
-// void Voiture::setPrixEssence(double nouveauEssence) {
-//     prix = nouveauEssence;
-// }
-//
-// double Voiture::getPrixEssence() {
-//     return prix;
-// }
-
-
-// HeureMinute.h
-#include <bits/ranges_algo.h>
-#ifndef HEUREMINUTE_H
-#define HEUREMINUTE_H
 #include <iostream>
-#include <cstdint> // int8_t için
-
-struct HeureMinute {
-    int8_t heure;
-    int8_t minute;
-
-    friend std::ostream &operator<<(std::ostream &os, const HeureMinute &hm);
-
-    friend std::istream &operator>>(std::istream &is, HeureMinute &hm);
-
-    friend bool operator<(const HeureMinute &lhs, const HeureMinute &rhs);
-
-    friend HeureMinute operator+(const HeureMinute &lhs, const HeureMinute &rhs);
-
-    // Prototip ekle: operator<<, operator>>, operator<, operator+
-};
-#endif
-
-#include "HeureMinutes.h"
-#include <iostream>
-#include <iomanip>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-ostream &operator<<(std::ostream &os, const HeureMinute &hm) {
-    os << setw(2) << setfill('0') << +hm.heure << ":"
-            << setw(2) << setfill('0') << +hm.minute;
-    return os;
+
+class Pays {
+private:
+    const string nom;
+    double population;
+    double superficie;
+
+public:
+    Pays(const string& n, double pop, double sup);
+
+    double getSuperficie() const;
+
+    double getPopulation() const;
+
+    const string& getNom() const;
+
+    double densite()const;
+};
+
+Pays::Pays(const string& n, double pop, double sup) : nom(n), population(pop),
+                                               superficie(sup) {
 }
 
-istream &operator>>(std::istream &is, HeureMinute &hm) {
-    int h, m;
-    char sep;
-
-    if (is >> h >> sep >> m) {
-        hm.heure = static_cast<int8_t>(h);
-        hm.minute = static_cast<int8_t>(m);
-    }
-    return is;
+double Pays::getSuperficie() const {
+    return this->superficie;
 }
 
-bool operator<(const HeureMinute &lhs, const HeureMinute &rhs) {
-    if (lhs.heure != rhs.heure) {
-        return lhs.heure < rhs.heure;
-    } else {
-        return lhs.minute < rhs.minute;
-    }
+double Pays::getPopulation() const {
+    return this->population;
 }
 
 
+double Pays::densite() const {
+    return population * 1000000 / superficie;
+}
 
-HeureMinute operator+(const HeureMinute &lhs, const HeureMinute &rhs) {
-int totalMinutes = (lhs.heure*60 + lhs.minute) + rhs.heure*60 +rhs.minute;
-    totalMinutes = totalMinutes %1440;
+const string& Pays::getNom() const {
+    return this->nom;
+}
 
-    return HeureMinute{static_cast<int8_t>(totalMinutes /60),
-    static_cast<int8_t>(totalMinutes%60)};
+int main() {
+    vector<Pays> pays{
+            {"Suisse", 8.121830, 41290},
+            {"France", 66.663766, 547030},
+            {"Italie", 61.855120, 301230},
+            {"Allemagne", 80.854408, 357021}
+    };
+
+    // 1) En büyük yüzölçümü
+    auto maxSuperficie = max_element(pays.begin(), pays.end(),[](const Pays& p1, const Pays&p2) {
+        return p1.getSuperficie() < p2.getSuperficie();
+    });
+    // 2) En fazla nüfus
+    auto maxPopulation =max_element(pays.begin(), pays.end(),[](const Pays& p1, const Pays& p2) {
+        return p1.getPopulation() < p2.getPopulation();
+    });
+
+    // 3) En büyük yoğunluk
+    auto maxDensite =max_element(pays.begin(), pays.end(),[](const Pays& p1, const Pays&p2) {
+        return p1.densite() < p2.densite();
+    });
+
+    cout << "Pays ayant la plus grande superficie               : "
+         << maxSuperficie->getNom() << endl;
+    cout << "Pays ayant le plus d'habitants                     : "
+         << maxPopulation->getNom() << endl;
+    cout << "Pays ayant la densite de population la plus grande : "
+         << maxDensite->getNom() << endl;
+
+    return 0;
 }
