afcc41d59851c875540fc0506e20900390e503fe
KerimGurbaz <kerim.grbz.01@gmail.com>
2025-12-11 12:02:16 +0100

    allocation dynamique

--- main.cpp
+++ main.cpp
@@ -102,41 +102,131 @@
 //     cout << somme_elements(v.begin()+2, v.begin()+5);
 // }
 
-#include <cmath>
-#include <vector>
-#include <algorithm>
+// #include <cmath>
+// #include <vector>
+// #include <algorithm>
+// #include <iostream>
+// using namespace std;
+//
+// template<typename Iterator>
+// typename Iterator::value_type somme_elements(Iterator first, Iterator last) {
+//     typename Iterator::value_type somme = 0;
+//     for (Iterator it = first; it != last; ++it) {
+//         somme += *it;
+//     }
+//     return somme;
+// }
+//
+//
+// template<typename T>
+// T maxElement(const vector<T> &v) {
+//     auto max = max_element(v.begin(), v.end(), [](const T &a, const T &b) {
+//         return a < b;
+//     });
+//     return *max;
+// }
+//
+//
+//
+//
+// int main() {
+//     vector<int> v{10, 20, 30, 40, 50, 60};
+//     cout << somme_elements(v.begin() + 2, v.begin() + 5);
+//     cout << endl;
+//     cout << maxElement(v);
+//
+//
+//     cout<<count_if(v.begin()+1, v.end()-1, [](int x) {
+//         return x%3 == 0;
+//     });
+// }
+#include "../../../../../../Program Files/JetBrains/CLion 2024.1.4/bin/mingw/x86_64-w64-mingw32/include/complex.h"
+#ifndef "INTARRAY_H"
+#define "INTARRAY_H"
+
+class IntArray {
+private:
+int *data; //pointeur
+    int size; //dizinin boyutu
+
+public:
+   IntArray(int s);
+    ~IntArray();
+    IntArray(const IntArray& other);
+    IntArray& operator = (const IntArray& other);
+
+    void set(int index, int value);
+    void afficher()const;
+};
+
+#endif
+
+
 #include <iostream>
+#include <algorithm>
+#include "IntArray.h"
+IntArray::Intarray(int s) : size(s) {
+    data = new int[size]();
+}
+
+IntArray::~IntArray() {
+    delete[]data;
+}
+
+IntArray::IntArray(const IntArray& other): size(other.size) {
+    data = new int[size];
+
+    std::copy(other.data, other.data+size, data);
+}
+
+IntArray& IntArray::operator = (const IntArray& other) {
+    if(this == &other) {
+        return *this;
+    }
+    delete[]data;
+    size = other.size;
+    data = new int[size];
+    std::copy(other.data, other.data+ size, data);
+    return *this;
+}
+
+void IntArray::set(int index, int value) {
+    if(index < size) {
+        data[index]=value;
+    }
+}
+
+void IntArray::afficher()const {
+    std::cout<<"[";
+    for(size_t i = 0; i<size; ++i) {
+        std::cout<<data[i]<<" ";
+    }
+    std::cout<<"]\n";
+}
+
+
+
+
+
 using namespace std;
 
-template<typename Iterator>
-typename Iterator::value_type somme_elements(Iterator first, Iterator last) {
-    typename Iterator::value_type somme = 0;
-    for (Iterator it = first; it != last; ++it) {
-        somme += *it;
-    }
-    return somme;
-}
-
-
-template<typename T>
-T maxElement(const vector<T> &v) {
-    auto max = max_element(v.begin(), v.end(), [](const T &a, const T &b) {
-        return a < b;
-    });
-    return *max;
-}
-
-
-
-
 int main() {
-    vector<int> v{10, 20, 30, 40, 50, 60};
-    cout << somme_elements(v.begin() + 2, v.begin() + 5);
-    cout << endl;
-    cout << maxElement(v);
+    IntArray t1(5); // 5 elemanlı, hepsi 0
+    t1.set(0, 10);  // 0. indekse 10 ata (bu metodu da yaz: void set(idx, val))
+    t1.set(1, 20);
 
+    cout << "T1: "; t1.afficher(); // [ 10 20 0 0 0 ]
 
-    cout<<count_if(v.begin()+1, v.end()-1, [](int x) {
-        return x%3 == 0;
-    });
+    {
+        IntArray t2 = t1; // Copy Constructor çağrılır
+        t2.set(0, 999);   // t2 değişti, t1 DEĞİŞMEMELİ!
+        cout << "T2: "; t2.afficher();
+    } // t2 burada yok olur (Destructor). t1 hala yaşamalı!
+
+    IntArray t3(2);
+    t3 = t1; // Assignment Operator çağrılır (t3'ün eski belleği silinmeli!)
+
+    cout << "T3: "; t3.afficher(); // T1 ile aynı olmalı
+
+    return 0;
 }
