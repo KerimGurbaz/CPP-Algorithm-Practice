809f8625da6a4b4094de954ee0607f13b7908b65
KerimGurbaz <kerim.grbz.01@gmail.com>
2025-12-20 22:45:01 +0100

    class exercices

--- main.cpp
+++ main.cpp
@@ -684,3 +684,53 @@ using namespace std;
 //     p.y = 10.3;
 //     cout << p.x << " -- " << p.y << endl;
 // }
+
+#include <iostream>
+
+using namespace std;
+
+class Point {
+private:
+    double x, y;
+public:
+    Point():x(0.0), y(0.0){}
+    Point(double xVal, double yVal): x(xVal), y(yVal){}
+    void setX(double x) {
+        this->x = x;
+    }
+    void setY(double y) {
+        this->y = y;
+    }
+    void afficher()const {
+        cout<<"("<<x<<","<<y<<")"<<endl;;
+
+    }
+    void deplacer(double dx, double dy) {
+        x+= dx;
+        y+= dy;
+    }
+
+    double getX()const {
+        return x;
+    }
+
+    double getY()const {
+        return y;
+    }
+};
+
+
+int main() {
+    Point centre;
+    centre.afficher();
+
+    Point p(1.2,2.4);
+    p.afficher();
+
+    p.deplacer(0.8, 0.6);
+    p.afficher();
+
+    p.setX(5.5);
+    p.setY(10.3);
+    cout << p.getX() << " -- " << p.getY() << endl;
+}
